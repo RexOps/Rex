e023af3214269615730179c2fd23a924a560ef9f
Jan <krimdomu@googlemail.com>
2011-06-25 21:26:40 +0200

    fixed backward compatibility with perl 5.8.x

--- lib/Rex/Commands/DB.pm
+++ lib/Rex/Commands/DB.pm
@@ -99,7 +99,7 @@ sub import {
 
    my ($class, $opt) = @_;
 
-   $dbh = DBI->connect($opt->{"dsn"}, $opt->{"user"}, $opt->{"password"} // "");  
+   $dbh = DBI->connect($opt->{"dsn"}, $opt->{"user"}, $opt->{"password"} || "");  
 
    my ($ns_register_to, $file, $line) = caller;   
 

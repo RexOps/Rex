c058f8857e5081756bec38975662663a88833f34
Jan <krimdomu@googlemail.com>
2011-07-17 23:58:50 +0200

    added support for hpacucli

--- lib/Rex/Inventory.pm
+++ lib/Rex/Inventory.pm
@@ -15,6 +15,8 @@ use Rex::Commands::Network;
 use Rex::Commands::Run;
 use Rex::Commands::Gather;
 
+use Rex::Inventory::HP::ACU;
+
 sub new {
    my $that = shift;
    my $proto = ref($that) || $that;
@@ -48,6 +50,41 @@ sub get {
    my @netstat    = netstat;
    my $default_gw = default_gateway;
 
+   my @raid_controller;
+   my @raid_logical_drives;
+   my %raid_shelfs;
+   if(my $hp_raid = Rex::Inventory::HP::ACU->get()) {
+      # hp raid entdeckt
+      for my $key (keys %{$hp_raid}) {
+         push(@raid_controller, {
+                     type => $hp_raid->{$key}->{"description"},
+                     model => $hp_raid->{$key}->{"model"},
+                     serial_number => $hp_raid->{$key}->{"serial_number"},
+                     cache_status => $hp_raid->{$key}->{"cache_status"},
+                  });
+
+         for my $shelf (keys %{$hp_raid->{$key}->{"array"}}) {
+            my $shelf_data = $hp_raid->{$key}->{"array"}->{$shelf};
+            $raid_shelfs{$shelf} = {
+                  type => $shelf_data->{"interface_type"},
+                  status => $shelf_data->{"status"},
+               };
+
+            for my $l_drive (keys %{$hp_raid->{$key}->{"array"}->{$shelf}->{"logical_drive"}}) {
+               my $l_drive_data = $hp_raid->{$key}->{"array"}->{$shelf}->{"logical_drive"}->{$l_drive};
+               my ($size) = ($l_drive_data->{"size"} =~ m/^([0-9\.]+)/);
+               push(@raid_logical_drives, {
+                     status => $l_drive_data->{"status"},
+                     raid_level => $l_drive_data->{"fault_tolerance"},
+                     size => sprintf("%i", $size * 1024 * 1024 * 1024),
+                     dev => $l_drive_data->{"disk_name"},
+                     shelf => $shelf,
+                  });
+            }
+         }
+      }
+   }
+
    return {
       base_board  => ($base_board?$base_board->get_all():{}),
       bios        => $bios->get_all(),
@@ -58,6 +95,11 @@ sub get {
       net         => sub { my $ret = []; push(@{$ret}, $_->get_all()) for @net_devs; return $ret; }->(),
       storage     => sub { my $ret = []; push(@{$ret}, $_->get_all()) for @storage; return $ret; }->(),
       volumes     => sub { my $ret = []; push(@{$ret}, $_->get_all()) for @volumes; return $ret; }->(),
+      raid        => {
+         controller => \@raid_controller,
+         shelfs => \%raid_shelfs,
+         drives => \@raid_logical_drives,
+      },
       configuration => {
          network => {
             routes                => \@routes,

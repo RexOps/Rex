e8699bb722e47124d7f93f1828bc254a46bbb3b1
Jan Gehring <krimdomu@googlemail.com>
2011-08-24 20:13:33 +0200

    added service support for gentoo

---
+++ lib/Rex/Service/Gentoo.pm
@@ -0,0 +1,98 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Service::Gentoo;
+
+use strict;
+use warnings;
+
+use Rex::Commands::Run;
+use Rex::Logger;
+
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   return $self;
+}
+
+sub start {
+   my($self, $service) = @_;
+
+   run "/etc/init.d/$service start";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub restart {
+   my($self, $service) = @_;
+
+   run "/etc/init.d/$service restart";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub stop {
+   my($self, $service) = @_;
+
+   run "/etc/init.d/$service stop";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub reload {
+   my($self, $service) = @_;
+
+   run "/etc/init.d/$service reload";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub status {
+   my($self, $service) = @_;
+
+   run "/etc/init.d/$service status";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub ensure {
+   my ($self, $service, $what) = @_;
+
+   if($what =~  /^stop/) {
+      $self->stop($service);
+      run "rc-update del $service";
+   }
+   elsif($what =~ /^start/) {
+      $self->start($service);
+      run "rc-update add $service";
+   }
+}
+
+1;

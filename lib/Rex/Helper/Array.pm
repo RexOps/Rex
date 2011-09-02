d45e1770e84ef4a43a6bfa6cbf9272a9db5fc9d0
Jan <krimdomu@googlemail.com>
2011-09-02 18:47:35 +0200

    array helper functions

---
+++ lib/Rex/Helper/Array.pm
@@ -0,0 +1,26 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package Rex::Helper::Array;
+   
+use strict;
+use warnings;
+
+require Exporter;
+use base qw(Exporter);
+use vars qw(@EXPORT);
+    
+@EXPORT = qw(array_uniq);
+
+sub array_uniq {
+   my (@array) = @_;
+
+   my %all = ();
+   @all{@array} = 1;
+   return keys %all;
+}
+
+1;

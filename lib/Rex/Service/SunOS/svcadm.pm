624651e0780db30f98fe74f5b514eeed1bbd963d
Jan <krimdomu@googlemail.com>
2011-09-02 15:54:42 +0200

    added svcadm service provider for SunOS

---
+++ lib/Rex/Service/SunOS/svcadm.pm
@@ -0,0 +1,97 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Service::SunOS::svcadm;
+
+use strict;
+use warnings;
+
+use Rex::Commands::Run;
+use Rex::Logger;
+use Rex::Commands::Fs;
+
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   return $self;
+}
+
+sub start {
+   my($self, $service) = @_;
+
+   run "svcadm enable $service";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub restart {
+   my($self, $service) = @_;
+
+   run "svcadm restart $service";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub stop {
+   my($self, $service) = @_;
+
+   run "svcadm disable $service";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub reload {
+   my($self, $service) = @_;
+
+   run "svcadm refresh $service";
+
+   if($? == 0) {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub status {
+   my($self, $service) = @_;
+
+   my ($state) = grep { $_=$1 if /state\s+([a-z]+)/ } run "svcs -l $service";
+
+   if($state eq "online") {
+      return 1;
+   }
+
+   return 0;
+}
+
+sub ensure {
+   my ($self, $service, $what) = @_;
+
+   if($what =~  /^stop/) {
+      $self->stop($service);
+   }
+   elsif($what =~ /^start/) {
+      $self->start($service);
+   }
+}
+
+1;

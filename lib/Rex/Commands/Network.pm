5a9df55b70d2b64e594bfc2ecea806c759f274e9
Jan <krimdomu@googlemail.com>
2011-09-05 20:49:43 +0200

    added freebsd support for network functions

--- lib/Rex/Commands/Network.pm
+++ lib/Rex/Commands/Network.pm
@@ -55,30 +55,80 @@ sub route {
 
    my $os = get_operating_system();
    
-   if($os =~ m/BSD/) {
-      Rex::Logger::info("route not supported under BSD");
-      return;
-   }
-
    my @ret = ();
 
    my @route = run "netstat -nr";  
    if($? != 0) {
       die("Error running netstat");
    }
-   shift @route; shift @route; # remove first 2 lines
 
-   for my $route_entry (@route) {
-      my ($dest, $gw, $genmask, $flags, $mss, $window, $irtt, $iface) = split(/\s+/, $route_entry, 8);
-      push(@ret, {
-         destination => $dest,
-         gateway     => $gw,
-         genmask     => $genmask,
-         flags       => $flags,
-         mss         => $mss,
-         irtt        => $irtt,
-         iface       => $iface,
-      });
+   if(is_freebsd) {
+      my ($in_v6, $in_v4);
+      for my $route_entry (@route) {
+         if($route_entry eq "Internet:") {
+            $in_v4 = 1;
+            next;
+         }
+
+         if($route_entry eq "Internet6:") {
+            $in_v6 = 1; $in_v4 = 0;
+            next;
+         }
+
+         if($route_entry =~ m/^$/) {
+            $in_v6 = 0; $in_v4 = 0;
+            next;
+         }
+
+         if($route_entry =~ m/^Destination/) {
+            next;
+         }
+
+         if($in_v4) {
+            my ($dest, $gw, $flags, $refs, $use, $netif, $expire) = split(/\s+/, $route_entry, 7);
+            push(@ret, {
+               destination => $dest,
+               gateway     => $gw,
+               flags       => $flags,
+               iface       => $netif,
+               refs        => $refs,
+               use         => $use,
+               expire      => $expire,
+            });
+
+            next;
+         }
+
+         if($in_v6) {
+            my ($dest, $gw, $flags, $netif, $expire) = split(/\s+/, $route_entry, 5);
+            push(@ret, {
+               destination => $dest,
+               gateway     => $gw,
+               flags       => $flags,
+               iface       => $netif,
+               expire      => $expire,
+            });
+         }
+
+      }
+   }
+   else {
+      # linux is default
+      shift @route; shift @route; # remove first 2 lines
+
+      for my $route_entry (@route) {
+         my ($dest, $gw, $genmask, $flags, $mss, $window, $irtt, $iface) = split(/\s+/, $route_entry, 8);
+         push(@ret, {
+            destination => $dest,
+            gateway     => $gw,
+            genmask     => $genmask,
+            flags       => $flags,
+            mss         => $mss,
+            irtt        => $irtt,
+            iface       => $iface,
+         });
+      }
+
    }
 
    return @ret;
@@ -94,11 +144,6 @@ sub default_gateway {
 
    my $os = get_operating_system();
    
-   if($os =~ m/BSD/) {
-      Rex::Logger::info("default_gateway not supported under BSD");
-      return;
-   }
-
    my ($new_default_gw) = @_;
 
    if($new_default_gw) {
@@ -118,7 +163,7 @@ sub default_gateway {
    else {
       my @route = route();
 
-      my ($default_route) = grep { $_->{"flags"} =~ m/UG/ && $_->{"destination"} eq "0.0.0.0" } @route;
+      my ($default_route) = grep { $_->{"flags"} =~ m/UG/ && ( $_->{"destination"} eq "0.0.0.0" || $_->{"destination"} eq "default" ) } @route;
       return $default_route->{"gateway"} if $default_route;
    }
 }
@@ -132,103 +177,160 @@ sub netstat {
 
    my $os = get_operating_system();
    
-   if($os =~ m/BSD/) {
-      Rex::Logger::info("netstat not supported under BSD");
-      return;
-   }
-
    my @ret;
 
-   my @netstat = run "netstat -nap";
-   if($? != 0) {
-      die("Error running netstat");
+   if($os =~ m/BSD/) {
+      my @netstat = run "netstat -na";
+
+      if($? != 0) {
+         die("Error running netstat");
+      }
+
+      shift @netstat;
+
+      my ($in_inet, $in_unix) = (0, 0);
+
+      for my $line (@netstat) {
+         if($line =~ m/^Proto\s*Recv/) {
+            $in_inet = 1;
+            next;
+         }
+
+         if($line =~ m/^Active UNIX/) {
+            $in_inet = 0; $in_unix = 1;
+            next;
+         }
+
+         if($line =~ m/^Address\s*Type/) {
+            next;
+         }
+
+         if($in_inet) {
+            my ($proto, $recvq, $sendq, $local_addr, $foreign_addr, $state) = split(/\s+/, $line, 6);
+            if($proto eq "tcp4") { $proto = "tcp"; }
+            push(@ret, {
+               proto        => $proto,
+               recvq        => $recvq,
+               sendq        => $sendq,
+               local_addr   => $local_addr,
+               foreign_addr => $foreign_addr,
+               state        => $state,
+            });
+            next;
+         }
+
+         if($in_unix) {
+             my ($address, $type, $recvq, $sendq, $inode, $conn, $refs, $nextref, $addr) = split(/\s+/, $line, 9);
+             push(@ret, {
+               proto        => "unix",
+               address      => $address,
+               refcnt       => $refs,
+               type         => $type,
+               inode        => $inode,
+               path         => $addr,
+               recvq        => $recvq,
+               sendq        => $sendq,
+               conn         => $conn,
+               nextref      => $nextref,
+            });
+        
+            next;
+         }
+      }
+
    }
-   my ($in_inet, $in_unix) = (0, 0);
-   for my $line (@netstat) {
-      if($in_inet == 1) { ++$in_inet; next; }
-      if($in_unix == 1) { ++$in_unix; next; }
-      if($line =~ m/^Active Internet/) {
-         $in_inet = 1;
-         next;
+   else {
+      my @netstat = run "netstat -nap";
+      if($? != 0) {
+         die("Error running netstat");
       }
+      my ($in_inet, $in_unix) = (0, 0);
+      for my $line (@netstat) {
+         if($in_inet == 1) { ++$in_inet; next; }
+         if($in_unix == 1) { ++$in_unix; next; }
+         if($line =~ m/^Active Internet/) {
+            $in_inet = 1;
+            next;
+         }
 
-      if($line =~ m/^Active UNIX/) {
-         $in_inet = 0;
-         $in_unix = 1;
-         next;
+         if($line =~ m/^Active UNIX/) {
+            $in_inet = 0;
+            $in_unix = 1;
+            next;
+         }
+
+         if($in_inet) {
+            my ($proto, $recvq, $sendq, $local_addr, $foreign_addr, $state, $pid_cmd);
+
+            unless($line =~ m/^udp/) {
+               # no state
+               ($proto, $recvq, $sendq, $local_addr, $foreign_addr, $state, $pid_cmd) = split(/\s+/, $line, 7);
+            }
+            else {
+               ($proto, $recvq, $sendq, $local_addr, $foreign_addr, $pid_cmd) = split(/\s+/, $line, 6);
+            }
+
+            $pid_cmd ||= "";
+
+            my ($pid, $cmd) = split(/\//, $pid_cmd, 2);
+            if($pid =~ m/^-/) {
+               $pid = "";
+            }
+            $cmd   ||= "";
+            $state ||= "";
+
+            $cmd =~ s/\s+$//;
+
+            push(@ret, {
+               proto        => $proto,
+               recvq        => $recvq,
+               sendq        => $sendq,
+               local_addr   => $local_addr,
+               foreign_addr => $foreign_addr,
+               state        => $state,
+               pid          => $pid,
+               command      => $cmd,
+            });
+            next;
+         }
+
+         if($in_unix) {
+
+            my ($proto, $refcnt, $flags, $type, $state, $inode, $pid, $cmd, $path);
+
+            if($line =~ m/^([a-z]+)\s+(\d+)\s+\[([^\]]+)\]\s+([a-z]+)\s+([a-z]+)?\s+(\d+)\s+(\d+)\/([^\s]+)\s+(.*)$/i) {
+               ($proto, $refcnt, $flags, $type, $state, $inode, $pid, $cmd, $path) 
+                  = ($line =~ m/^([a-z]+)\s+(\d+)\s+\[([^\]]+)\]\s+([a-z]+)\s+([a-z]+)?\s+(\d+)\s+(\d+)\/([^\s]+)\s+(.*)$/i);
+            }
+            else {
+               ($proto, $refcnt, $flags, $type, $state, $inode, $path) 
+                  = ($line =~ m/^([a-z]+)\s+(\d+)\s+\[([^\]]+)\]\s+([a-z]+)\s+([a-z]+)?\s+(\d+)\s+\-\s+(.*)$/i);
+
+               $pid = "";
+               $cmd = "";
+            }
+
+
+            $state =~ s/^\s|\s$//g if ($state);
+            $flags =~ s/\s+$//;
+            $cmd =~ s/\s+$//;
+
+            my $data = {
+               proto        => $proto,
+               refcnt       => $refcnt,
+               flags        => $flags,
+               type         => $type,
+               state        => $state,
+               inode        => $inode,
+               pid          => $pid,
+               command      => $cmd,
+               path         => $path,
+            };
+
+            push(@ret, $data);
+         }
+
       }
-
-      if($in_inet) {
-         my ($proto, $recvq, $sendq, $local_addr, $foreign_addr, $state, $pid_cmd);
-
-         unless($line =~ m/^udp/) {
-            # no state
-            ($proto, $recvq, $sendq, $local_addr, $foreign_addr, $state, $pid_cmd) = split(/\s+/, $line, 7);
-         }
-         else {
-            ($proto, $recvq, $sendq, $local_addr, $foreign_addr, $pid_cmd) = split(/\s+/, $line, 6);
-         }
-
-         $pid_cmd ||= "";
-
-         my ($pid, $cmd) = split(/\//, $pid_cmd, 2);
-         if($pid =~ m/^-/) {
-            $pid = "";
-         }
-         $cmd   ||= "";
-         $state ||= "";
-
-         $cmd =~ s/\s+$//;
-
-         push(@ret, {
-            proto        => $proto,
-            recvq        => $recvq,
-            sendq        => $sendq,
-            local_addr   => $local_addr,
-            foreign_addr => $foreign_addr,
-            state        => $state,
-            pid          => $pid,
-            command      => $cmd,
-         });
-         next;
-      }
-
-      if($in_unix) {
-
-         my ($proto, $refcnt, $flags, $type, $state, $inode, $pid, $cmd, $path);
-
-         if($line =~ m/^([a-z]+)\s+(\d+)\s+\[([^\]]+)\]\s+([a-z]+)\s+([a-z]+)?\s+(\d+)\s+(\d+)\/([^\s]+)\s+(.*)$/i) {
-            ($proto, $refcnt, $flags, $type, $state, $inode, $pid, $cmd, $path) 
-               = ($line =~ m/^([a-z]+)\s+(\d+)\s+\[([^\]]+)\]\s+([a-z]+)\s+([a-z]+)?\s+(\d+)\s+(\d+)\/([^\s]+)\s+(.*)$/i);
-         }
-         else {
-            ($proto, $refcnt, $flags, $type, $state, $inode, $path) 
-               = ($line =~ m/^([a-z]+)\s+(\d+)\s+\[([^\]]+)\]\s+([a-z]+)\s+([a-z]+)?\s+(\d+)\s+\-\s+(.*)$/i);
-
-            $pid = "";
-            $cmd = "";
-         }
-
-
-         $state =~ s/^\s|\s$//g if ($state);
-         $flags =~ s/\s+$//;
-         $cmd =~ s/\s+$//;
-
-         my $data = {
-            proto        => $proto,
-            refcnt       => $refcnt,
-            flags        => $flags,
-            type         => $type,
-            state        => $state,
-            inode        => $inode,
-            pid          => $pid,
-            command      => $cmd,
-            path         => $path,
-         };
-
-         push(@ret, $data);
-      }
-
    }
 
    return @ret;

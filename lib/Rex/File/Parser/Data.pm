dc953aa5926e04b41ba39e03ec0e6b975bb206f7
Jan <krimdomu@googlemail.com>
2011-09-25 00:42:09 +0200

    added file parser library

---
+++ lib/Rex/File/Parser/Data.pm
@@ -0,0 +1,58 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package Rex::File::Parser::Data;
+   
+use strict;
+use warnings;
+ 
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   return $self;
+}
+
+sub read {
+   my ($self, $file) = @_;
+   return $self->_read_file($file);
+}
+
+sub _read_file {
+   my ($self, $file) = @_;
+
+   my $content = "";
+
+   my $in_file = 0;
+   for my $line (@{$self->{"data"}}) {
+      chomp $line;
+
+      if($line eq "\@end") {
+         $in_file = 0;
+         next;
+      }
+
+      if($in_file) {
+         $content .= $line . $/;
+      }
+
+      if($line eq "\@$file") {
+         $in_file = 1;
+         next;
+      }
+   }
+
+   return $content;
+}
+
+sub get {
+   my ($self, $file) = @_;
+}
+
+1;

8c124c9168f832a96adab8a80ab8a441f7fbc64e
Jan <krimdomu@googlemail.com>
2012-01-13 22:12:39 +0100

    added suppor for rhel5 & rhel6

--- lib/Rex/Commands/Gather.pm
+++ lib/Rex/Commands/Gather.pm
@@ -195,7 +195,7 @@ sub is_freebsd {
 sub is_redhat {
    my $os = get_operating_system();
 
-   my @redhat_clones = ("Fedora", "Redhat", "CentOS", "Scientific");
+   my @redhat_clones = ("Fedora", "Redhat", "CentOS", "Scientific", "RedHatEnterpriseServer");
 
    if(grep { /$os/i } @redhat_clones) {
       return 1;

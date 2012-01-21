5f7c0d9c189cab24fb5791ddc111eceed0f6bf67
Jan <krimdomu@googlemail.com>
2012-01-21 12:59:42 +0100

    suppress warning

--- lib/Rex.pm
+++ lib/Rex.pm
@@ -82,7 +82,7 @@ use base qw(Exporter);
 use vars qw(@EXPORT $VERSION @CONNECTION_STACK);
 
 @EXPORT = qw($VERSION);
-$VERSION = "0.23.99.17";
+$VERSION = "0.23.99.18";
 
 sub push_connection {
    push @CONNECTION_STACK, $_[0];
--- lib/Rex/Pkg/Debian.pm
+++ lib/Rex/Pkg/Debian.pm
@@ -27,7 +27,7 @@ sub is_installed {
 
    Rex::Logger::debug("Checking if $pkg is installed");
 
-   run("dpkg -L $pkg");
+   run("dpkg -L $pkg 2>&1 >/dev/null");
 
    unless($? == 0) {
       Rex::Logger::debug("$pkg is NOT installed.");

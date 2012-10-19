77b3efed739b977ff15f615557daf52874a51b81
Tomohiro Hosaka <bokutin@bokut.in>
2012-10-19 09:57:31 +0900

    fix the bad pkg_info execution on freebsd.

--- lib/Rex/Pkg/FreeBSD.pm
+++ lib/Rex/Pkg/FreeBSD.pm
@@ -27,7 +27,7 @@ sub is_installed {
 
    Rex::Logger::debug("Checking if $pkg is installed");
 
-   run("pkg_info $pkg-*");
+   run("pkg_info $pkg-\\*");
 
    unless($? == 0) {
       Rex::Logger::debug("$pkg is NOT installed.");

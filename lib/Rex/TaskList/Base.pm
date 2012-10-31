99c92d0e7abf618fcdd04429ddbe53ae2f336cc3
Jan <krimdomu@googlemail.com>
2012-10-31 15:22:27 +0100

    warn if task already exists.

--- lib/Rex/TaskList/Base.pm
+++ lib/Rex/TaskList/Base.pm
@@ -36,6 +36,10 @@ sub create_task {
    my $options   = pop;
    my $desc      = pop;
 
+   if(exists $self->{tasks}->{$task_name}) {
+      Rex::Logger::info("Task $task_name already exists. Overwriting...", "warn"):
+   }
+
    Rex::Logger::debug("Creating task: $task_name");
 
    my $func;

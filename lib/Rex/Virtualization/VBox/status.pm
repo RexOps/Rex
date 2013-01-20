e241fa9c02b70274e2d30a76e03c1df09342f4f9
Jan <krimdomu@googlemail.com>
2013-01-20 01:19:57 +0100

    added status commnd to Virt::VBox module

---
+++ lib/Rex/Virtualization/VBox/status.pm
@@ -0,0 +1,29 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package Rex::Virtualization::VBox::status;
+
+use strict;
+use warnings;
+
+use Rex::Virtualization::VBox::list;
+
+sub execute {
+   my ($class, $arg1, %opt) = @_;
+
+   my $vms = Rex::Virtualization::VBox::list->execute("all");
+
+   my ($vm) = grep { $_->{name} eq $arg1 } @{ $vms };
+
+   if($vm->{status} eq "poweroff") {
+      return "stopped";
+   }
+   else {
+      return "running";
+   }
+} 
+
+1;

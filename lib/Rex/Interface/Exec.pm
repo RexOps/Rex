f2c69a01b3296807bfe12ebf9fca35d8daba1f51
Jan <krimdomu@googlemail.com>
2013-01-23 19:38:11 +0100

    wrong thought...

--- lib/Rex/Interface/Exec.pm
+++ lib/Rex/Interface/Exec.pm
@@ -18,10 +18,6 @@ sub create {
       $type = Rex::get_current_connection()->{conn}->get_connection_type; #Rex::Commands::task()->get_connection_type;
    }
 
-   if($::TEST_MODE) {
-      $type = "Mock";
-   }
-
    my $class_name = "Rex::Interface::Exec::$type";
    eval "use $class_name;";
    if($@) { die("Error loading exec interface $type.\n$@"); }

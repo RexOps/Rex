d2aa34ecaf1080d3633de3c489b02eb36820da36
Jan <krimdomu@googlemail.com>
2013-02-17 19:58:27 +0100

    crypt password

--- lib/Rex/Interface/Exec/Sudo.pm
+++ lib/Rex/Interface/Exec/Sudo.pm
@@ -12,6 +12,10 @@ use warnings;
 use Rex::Config;
 use Rex::Interface::Exec::Local;
 use Rex::Interface::Exec::SSH;
+
+use Rex::Interface::File::Local;
+use Rex::Interface::File::SSH;
+
 use Rex::Commands;
 
 sub new {
@@ -30,16 +34,33 @@ sub exec {
    if($path) { $path = "PATH=$path" }
    $path ||= "";
 
-   my $exec;
+   my ($exec, $file);
    if(Rex::is_ssh()) {
       $exec = Rex::Interface::Exec->create("SSH");
+      $file = Rex::Interface::File->create("SSH");
    }
    else {
       $exec = Rex::Interface::Exec->create("Local");
+      $file = Rex::Interface::File->create("Local");
    }
 
    my $sudo_password = task->get_sudo_password;
-   return $exec->exec("echo '$sudo_password' | sudo -p '' -S sh -c 'LC_ALL=C $path $cmd'");
+   my $random_string = get_random(length($sudo_password), 'a' .. 'z');
+   my $crypt = $sudo_password ^ $random_string;
+
+   my $random_file = "/tmp/" . get_random(16, 'a' .. 'z') . ".sudo.tmp";
+
+   $file->open('>', $random_file);
+   $file->write(qq~#!/usr/bin/perl
+unlink \$0;
+my \$rnd = '$random_string';
+print \$ARGV[0] ^ \$rnd;
+print "\\n"
+   ~);
+   $file->close;
+   
+
+   return $exec->exec("perl $random_file $crypt | sudo -p '' -S sh -c 'LC_ALL=C $path $cmd'");
 }
 
 1;

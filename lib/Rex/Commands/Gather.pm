99f1819ad14bd80c16dfb2df9819bc760944ab16
Jan <krimdomu@googlemail.com>
2013-07-13 19:06:21 +0200

    export is_openwrt function

--- lib/Rex/Commands/Gather.pm
+++ lib/Rex/Commands/Gather.pm
@@ -43,7 +43,7 @@ use base qw(Rex::Exporter);
 use vars qw(@EXPORT);
 
 @EXPORT = qw(operating_system_is network_interfaces memory get_operating_system operating_system operating_system_version
-               is_freebsd is_netbsd is_openbsd is_redhat is_linux is_bsd is_solaris is_suse is_debian is_mageia is_windows is_alt 
+               is_freebsd is_netbsd is_openbsd is_redhat is_linux is_bsd is_solaris is_suse is_debian is_mageia is_windows is_alt is_openwrt
                get_system_information dump_system_information);
 
 =item get_operating_system

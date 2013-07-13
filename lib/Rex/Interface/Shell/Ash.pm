57e0ff73291573d43a28464bff1aff3055d3250b
Jan <krimdomu@googlemail.com>
2013-07-13 19:07:37 +0200

    added ash shell

---
+++ lib/Rex/Interface/Shell/Ash.pm
@@ -0,0 +1,26 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package Rex::Interface::Shell::Ash;
+
+use strict;
+use warnings;
+
+use Rex::Interface::Shell::Bash;
+
+use base qw(Rex::Interface::Shell::Bash);
+
+sub new {
+    my $class = shift;
+    my $proto = ref($class) || $class;
+    my $self = $proto->SUPER::new(@_);
+
+    bless($self, $class);
+    
+    return $self;
+}
+
+1;

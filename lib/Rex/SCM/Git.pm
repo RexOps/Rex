fa6dedb3563abe6b0dfe2057e02ba66179ab69d1
Jon Gentle <atrodo@atrodo.org>
2013-07-29 12:47:59 -0400

    Use the cwd option of run instead of chdir since chdir isn't supported remotely

--- lib/Rex/SCM/Git.pm
+++ lib/Rex/SCM/Git.pm
@@ -40,15 +40,10 @@ sub checkout {
       Rex::Logger::debug($out);
 
       if(exists $checkout_opt->{"branch"}) {
-         my $cwd = getcwd;
-
          unless($checkout_to) {
             $checkout_to = [ split(/\//, $repo_info->{"url"}) ]->[-1];
             $checkout_to =~ s/\.git$//;
          }
-
-         Rex::Logger::debug("chdir: $checkout_to");
-         chdir($checkout_to);
 
          my $checkout_cmd = sprintf($CHECKOUT_COMMAND, $checkout_opt->{"branch"}, $checkout_opt->{"branch"});
          Rex::Logger::debug("checkout_cmd: $checkout_cmd");
@@ -56,17 +51,13 @@ sub checkout {
          Rex::Logger::info("switching to branch " . $checkout_opt->{"branch"});
 
 
-         $out = run "$checkout_cmd";
+         $out = run "$checkout_cmd", cwd => $checkout_to;
          Rex::Logger::debug($out);
-
-         Rex::Logger::debug("chdir: $cwd");
-         chdir($cwd);
       }
    }
    elsif(is_dir("$checkout_to/.git")) {
-      chdir "$checkout_to";
       my $branch = $checkout_opt->{"branch"} || "master";
-      run "git pull origin $branch";
+      run "git pull origin $branch", cwd => $checkout_to;
    }
    else {
       Rex::Logger::info("Error checking out repository.", "warn");

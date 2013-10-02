199ee3a7e0fc3d1951871870d6d8befc8f26b76c
Jan <krimdomu@googlemail.com>
2013-10-02 23:47:08 +0200

    added possibility to define the output format of say() function. - fixes #155

--- lib/Rex/Commands.pm
+++ lib/Rex/Commands.pm
@@ -121,7 +121,8 @@ use base qw(Rex::Exporter);
             set
             get
             before after around
-            logformat
+            logformat log_format
+            sayformat say_format
             connection
             auth
             FALSE TRUE
@@ -1104,6 +1105,8 @@ sub logformat {
    $Rex::Logger::format = $format;
 }
 
+sub log_format { logformat(@_); }
+
 =item connection
 
 This function returns the current connection object.
@@ -1419,9 +1422,84 @@ sub get_environments {
    return sort { $a cmp $b } keys %{$environments};
 }
 
+=item sayformat($format)
+
+You can define the format of the say() function.
+
+%D - The current date yyyy-mm-dd HH:mm:ss
+
+%h - The target host
+
+%p - The pid of the running process
+
+%s - The Logstring
+
+You can also define the following values:
+
+default - the default behaviour.
+
+asis - will print every single parameter in its own line. This is usefull if you want to print the output of a command.
+
+=cut
+
+sub sayformat {
+   my ($format) = @_;
+   Rex::Config->set_say_format($format);
+}
+
+sub say_format { sayformat(@_); }
+
 sub say {
+   my (@data) = @_;
+
    return unless defined $_[0];
-   print @_, "\n";
+
+   my $format = Rex::Config->get_say_format;
+   if(! defined $format || $format eq "default") {
+      print @_, "\n";
+      return;
+   }
+
+   if($format eq "asis") {
+      print join("\n", @_);
+      return;
+   }
+
+   for my $line (@data) {
+      print _format_string($format, $line) . "\n";
+   }
+
+}
+
+# %D - Date
+# %h - Host
+# %s - Logstring
+sub _format_string {
+   my ($format, $line) = @_;
+
+   my $date = _get_timestamp();
+   my $host = Rex::get_current_connection() ? Rex::get_current_connection()->{conn}->server : "<local>";
+   my $pid = $$;
+
+   $format =~ s/\%D/$date/gms;
+   $format =~ s/\%h/$host/gms;
+   $format =~ s/\%s/$line/gms;
+   $format =~ s/\%p/$pid/gms;
+
+   return $format;
+}
+
+sub _get_timestamp {
+   my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+   $mon++;
+   $year += 1900;
+
+   return "$year-" 
+               . sprintf("%02i", $mon) . "-" 
+               . sprintf("%02i", $mday) . " " 
+               . sprintf("%02i", $hour) . ":" 
+               . sprintf("%02i", $min) . ":" 
+               . sprintf("%02i", $sec);
 }
 
 sub TRUE {

720b9a763171695cc7e67f7beb2e177fb5ddaa7a
Jan <krimdomu@googlemail.com>
2013-11-03 16:26:40 +0100

    there are differences between shells

--- lib/Rex/Interface/Shell/Bash.pm
+++ lib/Rex/Interface/Shell/Bash.pm
@@ -60,12 +60,12 @@ sub exec {
     }
 
     if ($self->{source_profile}) {
-        $complete_cmd = ". ~/.profile &> /dev/null ; $complete_cmd";
+        $complete_cmd = ". ~/.profile >/dev/null 2>&1 ; $complete_cmd";
     }
 
 
     if ($self->{source_global_profile}) {
-        $complete_cmd = ". /etc/profile &> /dev/null ; $complete_cmd";
+        $complete_cmd = ". /etc/profile >/dev/null 2>&1 ; $complete_cmd";
     }
 
 

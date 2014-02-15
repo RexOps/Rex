c05e72ebcfaa377e25184ddf20aac2f7a9092ee5
Jan <krimdomu@googlemail.com>
2014-02-15 16:35:40 +0100

    fixed rexify does not create projects due to missing file error - #318

--- lib/Rex/Commands/File.pm
+++ lib/Rex/Commands/File.pm
@@ -148,12 +148,16 @@ sub template {
    elsif($file =~ m/^\@/) {
       my @caller = caller(0);
       my $file_path = Rex::get_module_path($caller[0]);
+
       if(! -f $file_path) {
          if(-f "$file_path/__module__.pm") {
             $file_path = "$file_path/__module__.pm";
          }
          elsif(-f "$file_path/Module.pm") {
             $file_path = "$file_path/Module.pm";
+         }
+         elsif(-f $caller[1]) {
+            $file_path = $caller[1];
          }
       }
       my $file_content = eval { local(@ARGV, $/) = ($file_path); <>; };

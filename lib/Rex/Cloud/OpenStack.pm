e1e7c954b304784a4485fce519880ad43dbaccc8
Ferenc Erki <erkiferenc@gmail.com>
2014-03-18 20:26:44 +0100

    Reindent

--- lib/Rex/Cloud/OpenStack.pm
+++ lib/Rex/Cloud/OpenStack.pm
@@ -13,146 +13,145 @@ use JSON::XS;
 use LWP::UserAgent;
 
 sub new {
-    my $that  = shift;
-    my $proto = ref($that) || $that;
-    my $self  = {@_};
+  my $that  = shift;
+  my $proto = ref($that) || $that;
+  my $self  = {@_};
 
-    bless( $self, $proto );
+  bless( $self, $proto );
 
-    $self->{_agent} = LWP::UserAgent->new;
+  $self->{_agent} = LWP::UserAgent->new;
 
-    return $self;
+  return $self;
 }
 
 sub set_auth {
-    my ( $self, %auth ) = @_;
+  my ( $self, %auth ) = @_;
 
-    $self->{auth} = \%auth;
+  $self->{auth} = \%auth;
 }
 
 sub _request {
-    my ( $self, $method, $url, @params ) = @_;
+  my ( $self, $method, $url, @params ) = @_;
 
-    my $response = $self->{_agent}->request( $method->( $url, @params ) );
+  my $response = $self->{_agent}->request( $method->( $url, @params ) );
 
-    return decode_json( $response->content ) if $response->content;
+  return decode_json( $response->content ) if $response->content;
 }
 
 sub _authenticate {
-    my $self = shift;
+  my $self = shift;
 
-    my $auth_data = {
-        auth => {
-            tenantName => $self->{auth}{tenantName} || '',
-            passwordCredentials => {
-                username => $self->{auth}{username},
-                password => $self->{auth}{password},
-            }
-        }
-    };
+  my $auth_data = {
+    auth => {
+      tenantName => $self->{auth}{tenantName} || '',
+      passwordCredentials => {
+        username => $self->{auth}{username},
+        password => $self->{auth}{password},
+      }
+    }
+  };
 
-    my $content = $self->_request(
-        POST         => $self->{__endpoint} . '/tokens',
-        content_type => 'application/json',
-        content      => encode_json($auth_data),
-    );
+  my $content = $self->_request(
+    POST         => $self->{__endpoint} . '/tokens',
+    content_type => 'application/json',
+    content      => encode_json($auth_data),
+  );
 
-    $self->{auth}{tokenId} = $content->{access}{token}{id};
+  $self->{auth}{tokenId} = $content->{access}{token}{id};
 
-    $self->{_agent}
-        ->default_header( 'X-Auth-Token' => $self->{auth}{tokenId} );
+  $self->{_agent}->default_header( 'X-Auth-Token' => $self->{auth}{tokenId} );
 
-    $self->{_catalog} = $content->{access}{serviceCatalog};
+  $self->{_catalog} = $content->{access}{serviceCatalog};
 }
 
 sub get_nova_url {
-    my $self = shift;
+  my $self = shift;
 
-    $self->_authenticate unless $self->{auth}{tokenId};
+  $self->_authenticate unless $self->{auth}{tokenId};
 
-    my @nova_services
-        = grep { $_->{type} eq 'compute' } @{ $self->{_catalog} };
-    return $nova_services[0]{endpoints}[0]{publicURL};
+  my @nova_services =
+    grep { $_->{type} eq 'compute' } @{ $self->{_catalog} };
+  return $nova_services[0]{endpoints}[0]{publicURL};
 }
 
 sub run_instance {
-    my ( $self, %data ) = @_;
-    my $nova_url     = $self->get_nova_url;
-    my $request_data = {
-        server => {
-            flavorRef => $data{plan_id},
-            imageRef  => $data{image_id},
-            name      => $data{name},
-        }
-    };
+  my ( $self, %data ) = @_;
+  my $nova_url     = $self->get_nova_url;
+  my $request_data = {
+    server => {
+      flavorRef => $data{plan_id},
+      imageRef  => $data{image_id},
+      name      => $data{name},
+    }
+  };
 
-    $self->_request(
-        POST         => $nova_url . '/servers',
-        content_type => 'application/json',
-        content      => encode_json($request_data),
-    );
+  $self->_request(
+    POST         => $nova_url . '/servers',
+    content_type => 'application/json',
+    content      => encode_json($request_data),
+  );
 }
 
 sub terminate_instance {
-    my ( $self, %data ) = @_;
-    my $nova_url = $self->get_nova_url;
+  my ( $self, %data ) = @_;
+  my $nova_url = $self->get_nova_url;
 
-    $self->_request( DELETE => $nova_url . '/servers/' . $data{instance_id} );
+  $self->_request( DELETE => $nova_url . '/servers/' . $data{instance_id} );
 }
 
 sub list_instances {
-    my $self     = shift;
-    my $nova_url = $self->get_nova_url;
-    my @instances;
+  my $self     = shift;
+  my $nova_url = $self->get_nova_url;
+  my @instances;
 
-    my $content = $self->_request( GET => $nova_url . '/servers/detail' );
+  my $content = $self->_request( GET => $nova_url . '/servers/detail' );
 
-    for my $instance ( @{ $content->{servers} } ) {
-        push @instances,
-            {
-            ip              => $instance->{addresses}{public}[0]{addr},
-            id              => $instance->{id},
-            architecture    => undef,
-            type            => $instance->{flavor}{id},
-            dns_name        => undef,
-            state           => $instance->{status},
-            launch_time     => $instance->{'OS-SRV-USG:launched_at'},
-            name            => $instance->{name},
-            private_ip      => $instance->{addresses}{private}[0]{addr},
-            security_groups => join ',',
-            map { $_->{name} } @{ $instance->{security_groups} },
-            };
-    }
+  for my $instance ( @{ $content->{servers} } ) {
+    push @instances,
+      {
+      ip              => $instance->{addresses}{public}[0]{addr},
+      id              => $instance->{id},
+      architecture    => undef,
+      type            => $instance->{flavor}{id},
+      dns_name        => undef,
+      state           => $instance->{status},
+      launch_time     => $instance->{'OS-SRV-USG:launched_at'},
+      name            => $instance->{name},
+      private_ip      => $instance->{addresses}{private}[0]{addr},
+      security_groups => join ',',
+      map { $_->{name} } @{ $instance->{security_groups} },
+      };
+  }
 
-    return @instances;
+  return @instances;
 }
 
 sub list_running_instances {
-    my $self = shift;
+  my $self = shift;
 
-    return grep { $_->{state} eq 'ACTIVE' } $self->list_instances;
+  return grep { $_->{state} eq 'ACTIVE' } $self->list_instances;
 }
 
 sub stop_instance {
-    my ( $self, %data ) = @_;
-    my $nova_url = $self->get_nova_url;
+  my ( $self, %data ) = @_;
+  my $nova_url = $self->get_nova_url;
 
-    $self->_request(
-        POST => $nova_url . '/servers/' . $data{instance_id} . '/action',
-        content_type => 'application/json',
-        content      => encode_json( { suspend => 'null' } ),
-    );
+  $self->_request(
+    POST         => $nova_url . '/servers/' . $data{instance_id} . '/action',
+    content_type => 'application/json',
+    content      => encode_json( { suspend => 'null' } ),
+  );
 }
 
 sub start_instance {
-    my ( $self, %data ) = @_;
-    my $nova_url = $self->get_nova_url;
+  my ( $self, %data ) = @_;
+  my $nova_url = $self->get_nova_url;
 
-    $self->_request(
-        POST => $nova_url . '/servers/' . $data{instance_id} . '/action',
-        content_type => 'application/json',
-        content      => encode_json( { resume => 'null' } ),
-    );
+  $self->_request(
+    POST         => $nova_url . '/servers/' . $data{instance_id} . '/action',
+    content_type => 'application/json',
+    content      => encode_json( { resume => 'null' } ),
+  );
 }
 
 1;

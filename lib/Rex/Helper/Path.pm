a9834ed589d905c1dd003ecf22a3738725df6ded
Ferenc Erki <erkiferenc@gmail.com>
2014-04-19 10:44:12 +0200

    perltidy

--- lib/Rex/Helper/Path.pm
+++ lib/Rex/Helper/Path.pm
@@ -1,9 +1,9 @@
 #
 # (c) Jan Gehring <jan.gehring@gmail.com>
-# 
+#
 # vim: set ts=2 sw=2 tw=0:
 # vim: set expandtab:
-  
+
 package Rex::Helper::Path;
 
 use strict;
@@ -29,13 +29,12 @@ use Rex::Interface::Exec;
 # RETURNS: module file
 #
 sub get_file_path {
-  my ($file_name, $caller_package, $caller_file) = @_;
+  my ( $file_name, $caller_package, $caller_file ) = @_;
 
   $file_name = resolv_path($file_name);
 
-
-  if(! $caller_package) {
-    ($caller_package, $caller_file) = caller();
+  if ( !$caller_package ) {
+    ( $caller_package, $caller_file ) = caller();
   }
 
   # check if a file in $BASE overwrites the module file
@@ -44,35 +43,35 @@ sub get_file_path {
   $::rexfile ||= $0;
 
   my @path_parts;
-  if($^O =~ m/^MSWin/ && ! Rex::is_ssh()) {
-    @path_parts = split(/\//, $::rexfile);
+  if ( $^O =~ m/^MSWin/ && !Rex::is_ssh() ) {
+    @path_parts = split( /\//, $::rexfile );
   }
   else {
-    @path_parts = split(/\//, realpath($::rexfile));
+    @path_parts = split( /\//, realpath($::rexfile) );
   }
   pop @path_parts;
 
-  my $real_path = join('/', @path_parts);
+  my $real_path = join( '/', @path_parts );
 
-  if(-e $file_name) {
+  if ( -e $file_name ) {
     return $file_name;
   }
 
-  if(-e $real_path . '/' . $file_name) {
+  if ( -e $real_path . '/' . $file_name ) {
     return $real_path . '/' . $file_name;
   }
 
   # walk down the wire to find the file...
   my ($old_caller_file) = $caller_file;
   my $i = 0;
-  while($caller_package && $i <= 50) {
-    ($caller_package, $caller_file) = caller($i);
-    if(! $caller_package) {
+  while ( $caller_package && $i <= 50 ) {
+    ( $caller_package, $caller_file ) = caller($i);
+    if ( !$caller_package ) {
       last;
     }
 
     my $module_path = Rex::get_module_path($caller_package);
-    if(-e "$module_path/$file_name") {
+    if ( -e "$module_path/$file_name" ) {
       $file_name = "$module_path/$file_name";
       return $file_name;
     }
@@ -88,28 +87,32 @@ sub get_file_path {
 sub get_tmp_file {
   my $rnd_file;
 
-  if(Rex::is_ssh()) {
-    $rnd_file = Rex::Config->get_tmp_dir . "/" . Rex::Commands::get_random(12, 'a' .. 'z') . ".tmp";
+  if ( Rex::is_ssh() ) {
+    $rnd_file = Rex::Config->get_tmp_dir . "/"
+      . Rex::Commands::get_random( 12, 'a' .. 'z' ) . ".tmp";
   }
-  elsif($^O =~ m/^MSWin/) {
+  elsif ( $^O =~ m/^MSWin/ ) {
     my $tmp_dir = Rex::Config->get_tmp_dir;
-    if($tmp_dir eq "/tmp") {
+    if ( $tmp_dir eq "/tmp" ) {
       $tmp_dir = $ENV{TMP};
     }
 
-    $rnd_file = $tmp_dir . "/" . Rex::Commands::get_random(12, 'a' .. 'z') . ".tmp"
+    $rnd_file =
+      $tmp_dir . "/" . Rex::Commands::get_random( 12, 'a' .. 'z' ) . ".tmp";
   }
   else {
-    $rnd_file = Rex::Config->get_tmp_dir . "/" . Rex::Commands::get_random(12, 'a' .. 'z') . ".tmp";
+    $rnd_file = Rex::Config->get_tmp_dir . "/"
+      . Rex::Commands::get_random( 12, 'a' .. 'z' ) . ".tmp";
   }
 
   return $rnd_file;
 }
 
 sub resolv_path {
-  my ($path, $local) = @_;
+  my ( $path, $local ) = @_;
 
-  if($path !~ m/^~/) {
+  if ( $path !~ m/^~/ ) {
+
     # path starts not with ~ so we don't need to expand $HOME.
     # just return it.
     return $path;
@@ -119,13 +122,14 @@ sub resolv_path {
   require Rex::User;
   my $user_o = Rex::User->get;
 
-  if($local) {
-    if($^O =~ m/^MSWin/) {
+  if ($local) {
+    if ( $^O =~ m/^MSWin/ ) {
+
       # windows path:
       $home_path = $ENV{'USERPROFILE'};
     }
     else {
-      if($path =~ m/^~([a-zA-Z0-9_][^\/]+)\//) {
+      if ( $path =~ m/^~([a-zA-Z0-9_][^\/]+)\// ) {
         my $user_name = $1;
         my %user_info = $user_o->get_user($user_name);
         $home_path = $user_info{home};
@@ -138,14 +142,14 @@ sub resolv_path {
     }
   }
   else {
-    if($path =~ m/^~([a-zA-Z0-9_][^\/]+)\//) {
+    if ( $path =~ m/^~([a-zA-Z0-9_][^\/]+)\// ) {
       my $user_name = $1;
       my %user_info = $user_o->get_user($user_name);
       $home_path = $user_info{home};
       $path =~ s/^~$user_name/$home_path/;
     }
     else {
-      my $exec = Rex::Interface::Exec->create;
+      my $exec        = Rex::Interface::Exec->create;
       my $remote_home = $exec->exec("echo \$HOME");
       $remote_home =~ s/[\r\n]//gms;
       $home_path = $remote_home;
--- t/helper_path.t
+++ t/helper_path.t
@@ -6,28 +6,40 @@ use Cwd 'getcwd';
 
 use_ok 'Rex::Helper::Path';
 
-$::rexfile = "Rexfile"; $::rexfile = "Rexfile";
+$::rexfile = "Rexfile";
+$::rexfile = "Rexfile";
 
-my $path = Rex::Helper::Path::get_file_path("files/foo.txt", "main", "Rexfile");
-ok($path eq "./files/foo.txt", "got file path if called from Rexfile");
+my $path =
+  Rex::Helper::Path::get_file_path( "files/foo.txt", "main", "Rexfile" );
+ok( $path eq "./files/foo.txt", "got file path if called from Rexfile" );
 
 my $cwd = getcwd;
-$path = Rex::Helper::Path::get_file_path("$cwd/Makefile.PL", "main", "Rexfile");
-ok($path eq "$cwd/Makefile.PL", "got file path if called from Rexfile - absolute path");
+$path =
+  Rex::Helper::Path::get_file_path( "$cwd/Makefile.PL", "main", "Rexfile" );
+ok( $path eq "$cwd/Makefile.PL",
+  "got file path if called from Rexfile - absolute path" );
 
-$path = Rex::Helper::Path::get_file_path("files/foo.txt", "main", "this/is/Rexfile");
-ok($path eq "this/is/files/foo.txt", "got file path if called Rexfile from other directory");
+$path = Rex::Helper::Path::get_file_path( "files/foo.txt", "main",
+  "this/is/Rexfile" );
+ok( $path eq "this/is/files/foo.txt",
+  "got file path if called Rexfile from other directory" );
 
-$path = Rex::Helper::Path::get_file_path("files/foo.txt", "main", "/this/is/Rexfile");
-ok($path eq "/this/is/files/foo.txt", "got file path if called Rexfile from other directory (absolute)");
+$path = Rex::Helper::Path::get_file_path( "files/foo.txt", "main",
+  "/this/is/Rexfile" );
+ok( $path eq "/this/is/files/foo.txt",
+  "got file path if called Rexfile from other directory (absolute)" );
 
-$path = Rex::Helper::Path::get_file_path("files/foo.txt", "File::Foo", "lib/File/Foo/__module__.pm");
-ok($path eq "lib/File/Foo/files/foo.txt", "got file path for File::Foo module");
+$path = Rex::Helper::Path::get_file_path( "files/foo.txt", "File::Foo",
+  "lib/File/Foo/__module__.pm" );
+ok(
+  $path eq "lib/File/Foo/files/foo.txt",
+  "got file path for File::Foo module"
+);
 
 $path = Rex::Helper::Path::get_tmp_file();
-if($^O =~ m/^MSWin/) {
-   ok($path =~ m/c:[\/\\]/i, "found windows directory");
+if ( $^O =~ m/^MSWin/ ) {
+  ok( $path =~ m/c:[\/\\]/i, "found windows directory" );
 }
 else {
-   ok($path =~ m/^\/tmp/);
+  ok( $path =~ m/^\/tmp/ );
 }

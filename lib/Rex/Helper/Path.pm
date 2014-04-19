8d9257deeb83ab9715032d474ec6e88297a2e345
Ferenc Erki <erkiferenc@gmail.com>
2014-04-19 10:51:23 +0200

    Simplify get_tmp_file

--- lib/Rex/Helper/Path.pm
+++ lib/Rex/Helper/Path.pm
@@ -9,6 +9,7 @@ package Rex::Helper::Path;
 use strict;
 use warnings;
 
+use File::Spec;
 use File::Basename qw(dirname);
 require Exporter;
 
@@ -85,27 +86,8 @@ sub get_file_path {
 }
 
 sub get_tmp_file {
-  my $rnd_file;
-
-  if ( Rex::is_ssh() ) {
-    $rnd_file = Rex::Config->get_tmp_dir . "/"
-      . Rex::Commands::get_random( 12, 'a' .. 'z' ) . ".tmp";
-  }
-  elsif ( $^O =~ m/^MSWin/ ) {
-    my $tmp_dir = Rex::Config->get_tmp_dir;
-    if ( $tmp_dir eq "/tmp" ) {
-      $tmp_dir = $ENV{TMP};
-    }
-
-    $rnd_file =
-      $tmp_dir . "/" . Rex::Commands::get_random( 12, 'a' .. 'z' ) . ".tmp";
-  }
-  else {
-    $rnd_file = Rex::Config->get_tmp_dir . "/"
-      . Rex::Commands::get_random( 12, 'a' .. 'z' ) . ".tmp";
-  }
-
-  return $rnd_file;
+  return File::Spec->join( Rex::Config->get_tmp_dir(),
+    Rex::Commands::get_random( 12, 'a' .. 'z' ) . '.tmp' );
 }
 
 sub resolv_path {

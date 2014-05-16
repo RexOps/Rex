c97976a76133316ff7504420beee58aab912b8ff
Jan <krimdomu@googlemail.com>
2014-05-16 18:39:32 +0200

    fixed a case where newlines are in the output

--- lib/Rex/Config.pm
+++ lib/Rex/Config.pm
@@ -220,6 +220,8 @@ sub get_tmp_dir {
       my ($out) =
         $exec->exec("perl -MFile::Spec -le 'print File::Spec->tmpdir'");
       chomp $out;
+      $out =~ s/[\r\n]//gms;
+
       if ( $? == 0 && $out ) {
         $cache->set( "tmpdir", $out );
         return $out;

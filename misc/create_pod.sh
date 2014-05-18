e755f9aba275b18e0b06453da73e206c80f4211e
Ferenc Erki <erkiferenc@gmail.com>
2014-05-18 16:04:35 +0200

    Remove duplicate entry from for loop

--- misc/create_pod.sh
+++ misc/create_pod.sh
@@ -19,8 +19,7 @@ EOF
 
 )
 
-for x in lib/Rex/Commands/Cloud.pm \
-   lib/Rex/Commands/Box.pm \
+for x in lib/Rex/Commands/Box.pm \
    lib/Rex/Commands/Cloud.pm \
    lib/Rex/Commands/Cron.pm \
    lib/Rex/Commands/DB.pm \

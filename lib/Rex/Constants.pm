0ec28ed49f37aaa732ef0c7e2ebb30e1ab74dba5
Jan <krimdomu@googlemail.com>
2014-06-02 15:54:16 +0200

    some constants

---
+++ lib/Rex/Constants.pm
@@ -0,0 +1,25 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package Rex::Constants;
+
+use strict;
+use warnings;
+
+require Exporter;
+use base qw(Exporter);
+use vars qw(@EXPORT);
+
+@EXPORT = qw(present absent latest running started stopped);
+
+sub present { return "present"; }
+sub absent { return "absent"; }
+sub latest { return "latest"; }
+sub running { return "running"; }
+sub started { return "started"; }
+sub stopped { return "stopped"; }
+
+1;

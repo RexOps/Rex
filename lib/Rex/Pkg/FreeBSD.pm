d0f67f0f3fe2773155d7dcd9b851a4182d8b2740
Jan <krimdomu@googlemail.com>
2014-07-05 12:31:22 +0200

    do the pkg check the other way round

--- lib/Rex/Pkg/FreeBSD.pm
+++ lib/Rex/Pkg/FreeBSD.pm
@@ -22,21 +22,21 @@ sub new {
 
   bless( $self, $proto );
 
-  i_run("which pkg");
+  i_run("which pkg_add");
   if ( $? == 0 ) {
-    $self->{commands} = {
-      install         => 'pkg install -q -y %s',
-      install_version => 'pkg install -q -y %s',
-      remove          => 'pkg remove -y %s',
-      query           => 'pkg info',
-    };
-  }
-  else {
     $self->{commands} = {
       install         => 'pkg_add -r %s',
       install_version => 'pkg_add -r %s',
       remove          => 'pkg_delete %s',
       query           => 'pkg_info',
+    };
+  }
+  else {
+    $self->{commands} = {
+      install         => 'pkg install -q -y %s',
+      install_version => 'pkg install -q -y %s',
+      remove          => 'pkg remove -y %s',
+      query           => 'pkg info',
     };
   }
 

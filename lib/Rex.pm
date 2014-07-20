996641648da5d47bf4b9a2faad8dcc460d48a89b
Jan <krimdomu@googlemail.com>
2014-07-20 22:08:28 +0200

    increment dev version

--- lib/Rex.pm
+++ lib/Rex.pm
@@ -70,7 +70,7 @@ use File::Basename;
 our ( @EXPORT, $VERSION, @CONNECTION_STACK, $GLOBAL_SUDO, $MODULE_PATHS,
   $WITH_EXIT_STATUS );
 
-$VERSION = "0.49.99.1";
+$VERSION = "0.49.99.2";
 $WITH_EXIT_STATUS = 1;   # since 0.50 activated by default
 
 my $cur_dir;

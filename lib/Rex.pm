a782afae002eb5105944e2732d19db8870624a4b
Jan <krimdomu@googlemail.com>
2014-08-30 11:59:30 +0200

    version

--- lib/Rex.pm
+++ lib/Rex.pm
@@ -75,6 +75,8 @@ our ( @EXPORT, $VERSION, @CONNECTION_STACK, $GLOBAL_SUDO, $MODULE_PATHS,
 
 $WITH_EXIT_STATUS = 1;    # since 0.50 activated by default
 
+$VERSION = "0.51.99.2";
+
 my $cur_dir;
 
 BEGIN {

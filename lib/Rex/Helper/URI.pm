9efe7961de53632a0c62a07a2311646865d8bfd2
Jan <krimdomu@googlemail.com>
2014-09-07 19:43:47 +0200

    use custom uri encode function

---
+++ lib/Rex/Helper/URI.pm
@@ -0,0 +1,23 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=2 sw=2 tw=0:
+# vim: set expandtab:
+   
+package Rex::Helper::URI;
+
+use strict;
+use warnings;
+
+sub encode {
+  my ($part) = @_;
+  $part =~ s/([^\w\-\.\@])/_encode_char($1)/eg;
+  return $part;
+}
+
+sub _encode_char {
+    my ($char) = @_;
+    return "%" . sprintf "%lx", ord($char);
+}
+
+1;

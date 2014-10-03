6b9933a672eb5eb41cedba4956c0ac34f33f57bd
Jan <krimdomu@googlemail.com>
2014-10-03 19:56:04 +0200

    systemd check with load

--- lib/Rex/Service/Redhat/systemd.pm
+++ lib/Rex/Service/Redhat/systemd.pm
@@ -31,7 +31,7 @@ sub new {
     status         => 'systemctl status %s >/dev/null',
     ensure_stop    => 'systemctl disable %s',
     ensure_start   => 'systemctl enable %s',
-    service_exists => 'systemctl | grep %s',
+    service_exists => 'systemctl show %s | grep LoadState=loaded',
   };
 
   return $self;

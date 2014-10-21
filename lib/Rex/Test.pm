30f2420e80bfbb1fc7b7819c8ce0586a74c49109
Ferenc Erki <erkiferenc@gmail.com>
2014-10-21 17:39:28 +0200

    Add parameter for Test:run to specify which tests to run

--- lib/Rex/Test.pm
+++ lib/Rex/Test.pm
@@ -10,18 +10,22 @@ use Rex -base;
 use Data::Dumper;
 use Rex::Commands::Box;
 
+desc 'Run tests specified with --test=testfile (default: t/*.t)';
 task run => make {
   Rex::Logger::info("Running integration tests...");
 
+  my $parameters = shift;
   my @files;
+
   LOCAL {
-    @files = grep { $_ =~ /\.t$/ } list_files 't';
+    @files =
+      defined $parameters->{test} ? glob( $parameters->{test} ) : glob('t/*.t');
   };
 
   for my $file (@files) {
-    Rex::Logger::info("Running test: t/$file.");
-    do "t/$file";
-    Rex::Logger::info( "Error running t/$file: $@", "error" ) if $@;
+    Rex::Logger::info("Running test: $file.");
+    do "$file";
+    Rex::Logger::info( "Error running $file: $@", "error" ) if $@;
   }
 };
 

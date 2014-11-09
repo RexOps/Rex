476de2ef9e115c3e2dd0073fa36944054c0a91ab
Ferenc Erki <erkiferenc@gmail.com>
2014-11-09 01:28:12 +0100

    Replace defined-or operator to restore perl-5.8.0 compatibility

--- lib/Rex/Virtualization/Docker/daemon.pm
+++ lib/Rex/Virtualization/Docker/daemon.pm
@@ -15,8 +15,8 @@ use Rex::Helper::Run;
 sub execute {
   my ( $class, %opt ) = @_;
 
-  my $bind = $opt{bind} // '0.0.0.0';
-  my $host = $opt{host} // 'unix:///var/run/docker.sock';
+  my $bind = defined $opt{bind} ? $opt{bind} : '0.0.0.0';
+  my $host = defined $opt{host} ? $opt{host} : 'unix:///var/run/docker.sock';
 
   Rex::Logger::debug("starting docker daemon");
 

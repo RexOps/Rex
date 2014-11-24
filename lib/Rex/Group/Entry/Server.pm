2c30eed8b2e7fb8961b023cc5e2c108144cb49a0
Jan <krimdomu@googlemail.com>
2014-11-24 17:50:07 +0100

    use i_run

--- lib/Rex/Group/Entry/Server.pm
+++ lib/Rex/Group/Entry/Server.pm
@@ -15,6 +15,7 @@ use Rex::Helper::System;
 use Rex::Config;
 use Rex::Interface::Exec;
 use Data::Dumper;
+require Rex::Helper::Run;
 
 use List::MoreUtils qw(uniq);
 
@@ -337,8 +338,7 @@ sub evaluate_hostname {
 sub test_perl {
   my ($self) = @_;
 
-  my $exec = Rex::Interface::Exec->create;
-  $exec->exec("which perl");
+  Rex::Helper::Run::i_run("which perl");
 
   if ( $? != 0 ) {
     return 0;

ad74aa2b09437fff0592de78d35afd3729c1f9c2
JJ Merelo <jjmerelo@gmail.com>
2014-12-10 13:28:28 +0100

    Adds LinuxMint

---
+++ lib/Rex/Pkg/LinuxMint.pm
@@ -0,0 +1,29 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+#
+# vim: set ts=2 sw=2 tw=0:
+# vim: set expandtab:
+
+use strict;
+
+package Rex::Pkg::LinuxMint;
+
+use warnings;
+
+use Rex::Pkg::Debian;
+use Rex::Commands::Run;
+use Rex::Commands::File;
+
+use base qw(Rex::Pkg::Debian);
+
+sub new {
+  my $that  = shift;
+  my $proto = ref($that) || $that;
+  my $self  = $proto->SUPER::new(@_);
+
+  bless( $self, $proto );
+
+  return $self;
+}
+
+1;

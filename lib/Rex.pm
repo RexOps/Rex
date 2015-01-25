ea0af32d71a8429c520e7ff9fb549fe9951dab8b
Jan <krimdomu@googlemail.com>
2015-01-25 14:43:41 +0100

    use Net::OpenSSH is now default. don't need feature flag 0.55

--- lib/Rex.pm
+++ lib/Rex.pm
@@ -465,6 +465,9 @@ sub import {
 
   my ( $register_to, $file, $line ) = caller;
 
+  # use Net::OpenSSH if present (default without feature flag)
+  Rex::Config->set_use_net_openssh_if_present(1);
+
   if ( $what eq "-base" || $what eq "base" || $what eq "-feature" ) {
     require Rex::Commands;
     Rex::Commands->import( register_in => $register_to );

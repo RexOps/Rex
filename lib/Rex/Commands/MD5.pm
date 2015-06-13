aeab925e2c2faf6f3e5951e313b1c29da29bc56d
Ferenc Erki <erkiferenc@gmail.com>
2015-06-13 16:17:55 +0200

    Refactor MD5 checksum calculation (fix #719)

--- lib/Rex/Commands/MD5.pm
+++ lib/Rex/Commands/MD5.pm
@@ -45,7 +45,7 @@ use vars qw(@EXPORT);
 
 =head2 md5($file)
 
-This function will return the md5 sum (hexadecimal) for the given file.
+This function will return the MD5 checksum (hexadecimal) for the given file.
 
  task "md5", "server01", sub {
    my $md5 = md5("/etc/passwd");
@@ -57,76 +57,33 @@ sub md5 {
   my ($file) = @_;
 
   my $fs = Rex::Interface::Fs->create;
+
   if ( $fs->is_file($file) ) {
+    Rex::Logger::debug("Calculating checksum (MD5) of $file");
 
-    Rex::Logger::debug("Calculating Checksum (md5) of $file");
+    my $command =
+      ( $^O =~ m/^MSWin/i && Rex::is_local() )
+      ? qq(perl -MDigest::MD5 -e "open my \$fh, '<', '$file' or die 'Cannot open $file'; \$fh->binmode; print Digest::MD5->new->addfile(\$fh)->hexdigest;")
+      : qq(perl -MDigest::MD5 -e 'open my \$fh, "<", "$file" or die "Cannot open $file"; \$fh->binmode; print Digest::MD5->new->addfile(\$fh)->hexdigest;');
 
-    my $exec = Rex::Interface::Exec->create;
-    my $md5;
+    my $md5 = i_run($command);
 
-    my $uname_cmd = $exec->can_run( ["uname"] );
-    my ( $os, $md5_cmd );
-
-    if ($uname_cmd) {
-      $os = $exec->exec("$uname_cmd -s");
-    }
-    else {
-      $os = "Unknown";
-    }
-    if ( $os =~ /bsd|darwin/i ) {
-      $md5 = $exec->exec("/sbin/md5 -q '$file'");
-    }
-    else {
-      $md5_cmd = $exec->can_run( ["md5sum"] );
-      if ($md5_cmd) {
-        ($md5) = split( /\s/, $exec->exec("$md5_cmd '$file'") );
-      }
-      else {
-
-        my $script = q|
-        use Digest::MD5;
-        my $ctx = Digest::MD5->new;
-        open my $fh, "<", $ARGV[0];
-        binmode $fh;
-        while(my $line = <$fh>) {
-          $ctx->add($line);
-        }
-        print $ctx->hexdigest . "\n";
-        |;
-
-        my $rnd_file = get_tmp_file;
-
-        my $fh = Rex::Interface::File->create;
-        $fh->open( ">", $rnd_file );
-        $fh->write($script);
-        $fh->close;
-
-        if ( Rex::is_local() && $^O =~ m/^MSWin/ ) {
-          $md5 = $exec->exec("perl $rnd_file \"$file\"");
-        }
-        else {
-          $md5 = i_run "perl $rnd_file '$file'";
-        }
-
-        unless ( $? == 0 ) {
-          Rex::Logger::info("Unable to get md5 sum of $file");
-          die("Unable to get md5 sum of $file");
-        }
-
-        Rex::Interface::Fs->create->unlink($rnd_file);
-      }
+    unless ( $? == 0 ) {
+      my $message = "Unable to get MD5 checksum of $file: $!";
+      Rex::Logger::info($message);
+      die($message);
     }
 
-    Rex::Logger::debug("MD5SUM ($file): $md5");
-    $md5 =~ s/[\r\n]//gms;
+    chomp $md5;
+
+    Rex::Logger::debug("MD5 checksum of $file: $md5");
+
     return $md5;
-
   }
   else {
-
-    Rex::Logger::debug("File $file not found.");
-    die("File $file not found");
-
+    my $message = "File not found: $file";
+    Rex::Logger::debug($message);
+    die($message);
   }
 }
 

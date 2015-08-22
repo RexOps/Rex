156180cb60c8530bb6d6a822cb6c4ced88eb120b
Ferenc Erki <erkiferenc@gmail.com>
2015-08-22 09:14:55 +0200

    Fix Gentoo service detection (fix #747)

--- lib/Rex/Service/Gentoo.pm
+++ lib/Rex/Service/Gentoo.pm
@@ -25,15 +25,16 @@ sub new {
   bless( $self, $proto );
 
   $self->{commands} = {
-    start          => '/etc/init.d/%s start',
-    restart        => '/etc/init.d/%s restart',
-    stop           => '/etc/init.d/%s stop',
-    reload         => '/etc/init.d/%s reload',
-    status         => '/etc/init.d/%s status',
-    ensure_stop    => 'rc-update del %s',
-    ensure_start   => 'rc-update add %s',
-    action         => '/etc/init.d/%s %s',
-    service_exists => 'rc-config list | grep "\s%s\s"',
+    start        => '/etc/init.d/%s start',
+    restart      => '/etc/init.d/%s restart',
+    stop         => '/etc/init.d/%s stop',
+    reload       => '/etc/init.d/%s reload',
+    status       => '/etc/init.d/%s status',
+    ensure_stop  => 'rc-update del %s',
+    ensure_start => 'rc-update add %s',
+    action       => '/etc/init.d/%s %s',
+    service_exists =>
+      q(rc-config --brief list | awk '{print $1}' | grep '^%s$'),
   };
 
   return $self;

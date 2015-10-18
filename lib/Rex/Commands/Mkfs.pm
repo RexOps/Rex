8b18a5acc9772387dc8b223a0d4074623a82690e
Peter H. Ezetta <protocall7@gmail.com>
2015-10-18 00:27:45 -0700

    start of refactor mkfs/mkswap into their own pm

---
+++ lib/Rex/Commands/Mkfs.pm
@@ -0,0 +1,32 @@
+package Rex::Command::Mkfs;
+
+use warnings;
+use strict;
+
+# VERSION
+
+require Rex::Exporter;
+use base qw(Rex::Exporter);
+use vars qw(@EXPORT);
+
+sub mkfs {
+    if ( exists $option{fstype} ) {
+        if ( can_run("mkfs.$option{fstype}") ) {
+            Rex::Logger::info(
+                "Creating filesystem $option{fstype} on /dev/$lv_path");
+            run "mkfs.$option{fstype} /dev/$lv_path";
+        }
+        else {
+            die("Can't format partition with $option{fstype}");
+        }
+    }
+}
+
+sub mkswap {
+    if ( $option{fstype} eq "swap" ) {
+        Rex::Logger::info("Creating swap space on /dev/$lv_path");
+        run "mkswap -f /dev/$lv_path";
+    }
+}
+
+1;

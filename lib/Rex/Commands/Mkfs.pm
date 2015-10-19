df422e7fe5b4c729aa9dbd8e272c29c71570aa0d
Peter H. Ezetta <protocall7@gmail.com>
2015-10-19 01:04:15 -0700

    code cleanup and addition of POD

--- lib/Rex/Commands/Mkfs.pm
+++ lib/Rex/Commands/Mkfs.pm
@@ -1,4 +1,20 @@
-package Rex::Command::Mkfs;
+=head1 NAME
+
+Rex::Commands::Mkfs - Create filesystems
+
+=head1 DESCRIPTION
+
+With this module you can create filesystems on existing partitions and logical volumes.
+
+=head1 SYNOPSIS
+
+ use Rex::Commands::Mkfs;
+
+=head1 EXPORTED FUNCTIONS
+
+=cut
+
+package Rex::Commands::Mkfs;
 
 use warnings;
 use strict;
@@ -12,51 +28,64 @@ use vars qw(@EXPORT);
 @EXPORT = qw(mkfs mkswap);
 
 use Rex::Commands::Run;
+use Carp;
+
+=head2 mkfs($devname, %option)
+
+Create a filesystem on device $devname.
+
+ mkfs "sda1",
+   fstype => "ext3",
+   label  => "mydisk";
+
+=cut
 
 sub mkfs {
-    my ($lvname, %option) = @_;
-
-    unless ( ( defined $option{ondisk} ) xor( defined $option{onvg} ) ) {
-        die('You have to specify exactly one of ondisk or onvg options.');
-    }
-
-    unless ( $lvname =~ m/^[a-z0-9\-\._]+$/i ) {
-        die("Error in lvname. Allowed characters a-z, 0-9 and _-. .");
-    }
+    my ( $devname, %option ) = @_;
 
     if ( exists $option{fstype} ) {
         if ( can_run("mkfs.$option{fstype}") ) {
-            if ( defined $option{onvg} ) {
-                Rex::Logger::info(
-                    "Creating filesystem $option{fstype} on /dev/$lv_path");
-                run "mkfs.$option{fstype} /dev/$lv_path";
+            Rex::Logger::info(
+                "Creating filesystem $option{fstype} on /dev/$devname");
+
+            my $add_opts = "";
+
+            if ( exists $option{label} || exists $option{lable} ) {
+                my $label = $option{label} || $option{lable};
+                $add_opts .= " -L $label ";
             }
-            elsif ( defined $option{ondisk} ) {
-                Rex::Logger::info(
-                    "Creating filesystem $option{fstype} on /dev/$disk$part_num"
-                );
 
-                my $add_opts = "";
-
-                if ( exists $option{label} || exists $option{lable} ) {
-                    my $label = $option{label} || $option{lable};
-                    $add_opts .= " -L $label ";
-                }
-
-                run "mkfs.$option{fstype} $add_opts /dev/$disk$part_num";
-            }
+            run "mkfs.$option{fstype} $add_opts /dev/$devname";
         }
     }
     else {
         die("Can't format partition with $option{fstype}");
     }
+
+    return;
 }
 
+=head2 mkswap($devname, %option)
+
+Create swap space on device $devname.
+
+ mkfs "sda2",
+   fstype => "swap";
+
+=cut
+
 sub mkswap {
+    my ( $devname, %option ) = @_;
+
     if ( $option{fstype} eq "swap" ) {
         Rex::Logger::info("Creating swap space on /dev/$lv_path");
-        run "mkswap -f /dev/$lv_path";
+        run "mkswap -f /dev/$devname";
     }
+	else {
+		croak "Can't create swap partition");
+	}
+
+    return;
 }
 
 1;

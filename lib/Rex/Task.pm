RexOps/Rex

commit b0416b1710611b900284ba5a2d9d74991fc4351b

added ParamLookup and registering task parameter as template variables.

lib/Rex/Task.pm
@@ -624,7 +624,7 @@ sub connect {
       ssh      => $self->connection->get_connection_object,
       server   => $server,
       cache    => Rex::Interface::Cache->create(),
-      task     => $self,
+      task     => [],
       profiler => $profiler,
       reporter => Rex::Report->create( Rex::Config->get_report_type ),
       notify   => Rex::Notify->new(),
@@ -757,35 +757,55 @@ sub run {
     # this is a method call
     # so run the task
 
-    my $in_transaction = $options{in_transaction};
+    # TODO: refactor complete task calling
+    #       direct call with function and normal task call
 
-    $self->run_hook( \$server, "before" );
-    $self->connect($server);
+    my ( $in_transaction, $start_time );
 
-    my $start_time = time;
+    push @{ Rex::get_current_connection->{task} }, $self;
 
-    if ( Rex::Args->is_opt("c") ) {
+    $start_time = time;
 
-      # get and cache all os info
-      if ( !Rex::get_cache()->load() ) {
-        Rex::Logger::debug("No cache found, need to collect new data.");
-        $server->gather_information;
+    if ( $server ne "<func>" ) {
+
+      # this is _not_ a task call via function syntax.
+
+      $in_transaction = $options{in_transaction};
+
+      $self->run_hook( \$server, "before" );
+      $self->connect($server);
+
+      if ( Rex::Args->is_opt("c") ) {
+
+        # get and cache all os info
+        if ( !Rex::get_cache()->load() ) {
+          Rex::Logger::debug("No cache found, need to collect new data.");
+          $server->gather_information;
+        }
       }
-    }
 
-    if ( !$server->test_perl ) {
-      Rex::Logger::info(
-        "There is no perl interpreter found on this system. Some commands may not work. Sudo won't work.",
-        "warn"
-      );
-      sleep 3;
+      if ( !$server->test_perl ) {
+        Rex::Logger::info(
+          "There is no perl interpreter found on this system. Some commands may not work. Sudo won't work.",
+          "warn"
+        );
+        sleep 3;
+      }
+
     }
 
     # execute code
-    my $ret;
+    my @ret;
+    my $wantarray = wantarray;
 
     eval {
-      $ret = $self->executor->exec( $options{params}, $options{args} );
+      $self->{__task_parameters__} = $options{params};
+      if ($wantarray) {
+        @ret = $self->executor->exec( $options{params}, $options{args} );
+      }
+      else {
+        $ret[0] = $self->executor->exec( $options{params}, $options{args} );
+      }
       my $notify = Rex::get_current_connection()->{notify};
       $notify->run_postponed();
     } or do {
@@ -808,24 +828,34 @@ sub run {
       }
     };
 
-    if ( Rex::Args->is_opt("c") ) {
+    if ( $server ne "<func>" ) {
+      if ( Rex::Args->is_opt("c") ) {
 
-      # get and cache all os info
-      Rex::get_cache()->save();
+        # get and cache all os info
+        Rex::get_cache()->save();
+      }
+
+      Rex::get_current_connection()->{reporter}->report_task_execution(
+        failed     => 0,
+        start_time => $start_time,
+        end_time   => time,
+      );
+
+      Rex::get_current_connection()->{reporter}->write_report();
+
+      $self->disconnect($server) unless ($in_transaction);
+      $self->run_hook( \$server, "after" );
+
     }
 
-    Rex::get_current_connection()->{reporter}->report_task_execution(
-      failed     => 0,
-      start_time => $start_time,
-      end_time   => time,
-    );
+    pop @{ Rex::get_current_connection->{task} };
 
-    Rex::get_current_connection()->{reporter}->write_report();
-
-    $self->disconnect($server) unless ($in_transaction);
-    $self->run_hook( \$server, "after" );
-
-    return $ret;
+    if ($wantarray) {
+      return @ret;
+    }
+    else {
+      return $ret[0];
+    }
   }
 
   else {
@@ -839,6 +869,16 @@ sub run {
     # this is a deprecated static call
     Rex::TaskList->create()->run( $task, params => $params );
   }
+}
+
+sub set_parameter {
+  my ( $self, $key, $value ) = @_;
+  $self->{__task_parameters__}->{$key} = $value;
+}
+
+sub get_all_parameters {
+  my ($self) = @_;
+  return $self->{__task_parameters__};
 }
 
 sub modify_task {

lib/Rex.pm
@@ -527,6 +527,8 @@ sub import {
     require Rex::Commands::User;
     Rex::Commands::User->import( register_in => $register_to );
 
+    require Rex::Helper::Rexfile::ParamLookup;
+    Rex::Helper::Rexfile::ParamLookup->import( register_in => $register_to );
   }
 
   if ( $what eq "-feature" || $what eq "feature" ) {

lib/Rex/Commands.pm
@@ -241,7 +241,7 @@ sub task {
 
   if ( !@_ ) {
     if ( my $t = Rex::get_current_connection ) {
-      return $t->{task};
+      return $t->{task}->[-1];
     }
     return;
   }
@@ -321,112 +321,31 @@ sub task {
   use strict;
   use warnings;
 
+  $options->{'dont_register'} ||= $dont_register_tasks;
+  my $task_o = Rex::TaskList->create()->create_task( $task_name, @_, $options );
+
   if (!$class->can($task_name_save)
     && $task_name_save =~ m/^[a-zA-Z_][a-zA-Z0-9_]+$/ )
   {
     no strict 'refs';
     Rex::Logger::debug("Registering task: ${class}::$task_name_save");
-
     my $code = $_[-2];
     *{"${class}::$task_name_save"} = sub {
       Rex::Logger::info("Running task $task_name_save on current connection");
+      my $param;
 
-      Rex::Hook::run_hook( task => "before_execute", $task_name_save, @_ );
-
-      if ( Rex::Config->get_task_call_by_method
-        && $_[0]
-        && $_[0] =~ m/^[A-Za-z0-9_:]+$/
-        && ref $_[1] eq "HASH" )
-      {
-        shift;
-      }
-
-      my @ret;
-      if ( ref( $_[0] ) eq "HASH" ) {
-        if (wantarray) {
-          @ret = $code->(@_);
-        }
-        else {
-          my $t = $code->(@_);
-          @ret = ($t);
-        }
+      if ( $REGISTER_SUB_HASH_PARAMETER && scalar @_ % 2 == 0 && !ref $_[0] ) {
+        $param = {@_};
       }
       else {
-        if ( $REGISTER_SUB_HASH_PARAMETER && scalar @_ % 2 == 0 ) {
-          if (wantarray) {
-            @ret = $code->( {@_} );
-          }
-          else {
-            my $t = $code->( {@_} );
-            @ret = ($t);
-          }
-        }
-        else {
-          if (wantarray) {
-            @ret = $code->(@_);
-          }
-          else {
-            my $t = $code->(@_);
-            @ret = ($t);
-          }
-        }
+        $param = $_[0];
       }
 
-      Rex::Hook::run_hook( task => "after_execute", $task_name_save, @_ );
-
-      if (wantarray) {
-        return @ret;
-      }
-      else {
-        return $ret[0];
-      }
+      $task_o->run( "<func>", params => $param );
     };
-    use strict;
-  }
-  elsif ( ( $class ne "main" && $class ne "Rex::CLI" )
-    && !$class->can($task_name_save)
-    && $task_name_save =~ m/^[a-zA-Z_][a-zA-Z0-9_]+$/ )
-  {
-    # if not in main namespace, register the task as a sub
-    no strict 'refs';
-    Rex::Logger::debug(
-      "Registering task (not main namespace): ${class}::$task_name_save");
-    my $code = $_[-2];
-    *{"${class}::$task_name_save"} = sub {
-      Rex::Logger::info("Running task $task_name_save on current connection");
-
-      Rex::Hook::run_hook( task => "before_execute", $task_name_save, @_ );
-
-      my @ret;
-      if ( ref( $_[0] ) eq "HASH" ) {
-        if (wantarray) {
-          @ret = $code->(@_);
-        }
-        else {
-          my $t = $code->(@_);
-          @ret = ($t);
-        }
-      }
-      else {
-        if (wantarray) {
-          @ret = $code->( {@_} );
-        }
-        else {
-          my $t = $code->( {@_} );
-          @ret = ($t);
-        }
-      }
-
-      Rex::Hook::run_hook( task => "after_execute", $task_name_save, @_ );
-
-      return @ret;
-    };
-
-    use strict;
   }
 
-  $options->{'dont_register'} ||= $dont_register_tasks;
-  Rex::TaskList->create()->create_task( $task_name, @_, $options );
+  return $task_o;
 }
 
 =head2 desc($description)

lib/Rex/Config.pm
@@ -785,7 +785,13 @@ sub set_template_function {
 
 sub get_template_function {
   if ( ref($template_function) eq "CODE" ) {
-    return $template_function;
+    return sub {
+      my ( $content, $template_vars ) = @_;
+      $template_vars =
+        { %{ Rex::Commands::task()->get_all_parameters }, %{$template_vars} }
+        if ( Rex::Commands::task() );
+      return $template_function->( $content, $template_vars );
+    };
   }
 
   if ( Rex::Template::NG->is_loadable && get_use_template_ng() ) {
@@ -793,6 +799,9 @@ sub get_template_function {
     # new template engine
     return sub {
       my ( $content, $template_vars ) = @_;
+      $template_vars =
+        { %{ Rex::Commands::task()->get_all_parameters }, %{$template_vars} }
+        if ( Rex::Commands::task() );
       Rex::Template::NG->require;
       my $t = Rex::Template::NG->new;
       return $t->parse( $content, %{$template_vars} );
@@ -801,6 +810,9 @@ sub get_template_function {
 
   return sub {
     my ( $content, $template_vars ) = @_;
+    $template_vars =
+      { %{ Rex::Commands::task()->get_all_parameters }, %{$template_vars} }
+      if ( Rex::Commands::task() );
     use Rex::Template;
     my $template = Rex::Template->new;
     return $template->parse( $content, $template_vars );

lib/Rex/Helper/Rexfile/ParamLookup.pm
@@ -1,0 +1,102 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+#
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Helper::Rexfile::ParamLookup;
+
+use strict;
+use warnings;
+
+# VERSION
+
+use Devel::Caller;
+use Data::Dumper;
+require Rex::Exporter;
+require Rex::Commands;
+
+use base qw(Rex::Exporter);
+use vars qw (@EXPORT);
+
+@EXPORT = qw(param_lookup);
+
+sub param_lookup {
+  my ( $key, $default ) = @_;
+
+  my $ret;
+
+  my ($caller_pkg) = caller(0);
+
+  my @args = Devel::Caller::caller_args(1);
+  if ( ref $args[0] eq "HASH" ) {
+    if ( exists $args[0]->{$key} ) {
+      $ret = $args[0]->{$key};
+    }
+  }
+
+  if ( !$ret ) {
+
+    # check if cmdb is loaded
+    my ($use_cmdb) = grep { m/CMDB\.pm/ } keys %INC;
+    if ($use_cmdb) {
+
+      # look inside cmdb
+      my $cmdb_key = "${caller_pkg}::$key";
+      $ret = Rex::Commands::get( Rex::CMDB::cmdb($cmdb_key) );
+
+      if ( !$ret ) {
+
+        # check in resource
+        if ($Rex::Resource::INSIDE_RES) {
+          $cmdb_key =
+              "${caller_pkg}::"
+            . $Rex::Resource::CURRENT_RES[-1]->display_name
+            . "::$key";
+          $ret = Rex::Commands::get( Rex::CMDB::cmdb($cmdb_key) );
+        }
+      }
+
+      if ( !$ret ) {
+
+        # check in global namespace
+        $ret = Rex::Commands::get( Rex::CMDB::cmdb($key) );
+      }
+    }
+  }
+
+  if ( !$ret ) {
+    $ret = $default;
+  }
+
+  Rex::Commands::task()->set_parameter( $key => $ret );
+
+  return $ret;
+}
+
+1;
+
+=pod
+
+=head1 NAME
+
+Rex::Helper::Rexfile::ParamLookup - A command to manage task parameters.
+
+A command to manage task parameters. Additionally it register the parameters as template values.
+
+This module also looks inside a CMDB (if present) for a valid key.
+
+
+=head1 SYNOPSIS
+
+ task "setup", sub {
+   my $var = param_lookup "param_name", "default_value";
+ };
+
+=head1 LOOKUP
+
+First I<param_lookup> checks the task parameters for a valid parameter. If none is found and if a CMDB is used, it will look inside the cmdb.
+
+If your module is named "Rex::NTP" than it will first look if the key "Rex::NTP::param_name" exists. If it doesn't exists it checks for the key "param_name".
+
+=cut

lib/Rex/Interface/Executor/Default.pm
@@ -37,13 +37,20 @@ sub exec {
 
   Rex::Logger::debug( "Executing " . $task->name );
 
-  my $ret;
+  my $wantarray = wantarray;
+
+  my @ret;
   eval {
     my $code = $task->code;
 
     Rex::Hook::run_hook( task => "before_execute", $task->name, @_ );
 
-    $ret = $code->( $opts, $args );
+    if ($wantarray) {
+      @ret = $code->( $opts, $args );
+    }
+    else {
+      $ret[0] = $code->( $opts, $args );
+    }
 
     Rex::Hook::run_hook( task => "after_execute", $task->name, @_ );
   };
@@ -67,7 +74,12 @@ sub exec {
     }
   }
 
-  return $ret;
+  if ($wantarray) {
+    return @ret;
+  }
+  else {
+    return $ret[0];
+  }
 }
 
 1;

lib/Rex/TaskList/Base.pm
@@ -227,6 +227,7 @@ sub create_task {
 
   $self->{tasks}->{$task_name} = Rex::Task->new(%task_hash);
 
+  return $self->{tasks}->{$task_name};
 }
 
 sub get_tasks {

t/cmdb.t
@@ -69,13 +69,13 @@ set(
 );
 
 my $foo_all = get( cmdb( undef, "foo" ) );
-
 is_deeply(
   $foo_all,
   {
     'ntp'    => [ 'ntp1',            'ntp2' ],
     'newntp' => [ 'ntpdefaultfoo01', 'ntpdefaultfoo02', 'ntp1', 'ntp2' ],
     'dns'    => [ '1.1.1.1',         '2.2.2.2' ],
+    'MyTest::foo::mode' => '0666',
     'vhost' => {
       'name'     => 'foohost',
       'doc_root' => '/var/www'

commit 86dde8434a7331ef60472e6ab9c61d93a6583621

fixed a bug that was introduced by b0416b1710611b900284ba5a2d9d74991fc4351b

lib/Rex/Task.pm
@@ -776,6 +776,7 @@ sub run {
 
       $self->run_hook( \$server, "before" );
       $self->connect($server);
+      push @{ Rex::get_current_connection()->{task} }, $self;
 
       if ( Rex::Args->is_opt("c") ) {
 
@@ -795,8 +796,9 @@ sub run {
       }
 
     }
-
-    push @{ Rex::get_current_connection()->{task} }, $self;
+    else {
+      push @{ Rex::get_current_connection()->{task} }, $self;
+    }
 
     # execute code
     my @ret;
@@ -828,11 +830,10 @@ sub run {
 
         Rex::get_current_connection()->{reporter}->write_report();
 
+        pop @{ Rex::get_current_connection()->{task} };
         die($error);
       }
     };
-
-    pop @{ Rex::get_current_connection()->{task} };
 
     if ( $server ne "<func>" ) {
       if ( Rex::Args->is_opt("c") ) {
@@ -849,9 +850,14 @@ sub run {
 
       Rex::get_current_connection()->{reporter}->write_report();
 
+      pop @{ Rex::get_current_connection()->{task} };
+
       $self->disconnect($server) unless ($in_transaction);
       $self->run_hook( \$server, "after" );
 
+    }
+    else {
+      pop @{ Rex::get_current_connection()->{task} };
     }
 
     if ($wantarray) {
c6502c9393d026690b731a2bb243711b61c7f5dc
Jan <krimdomu@googlemail.com>
2015-11-20 23:51:32 +0100

    don't print summary on rex -T

--- lib/Rex/CLI.pm
+++ lib/Rex/CLI.pm
@@ -775,6 +775,9 @@ sub _list_groups {
 }
 
 sub summarize {
+  my %opts = Rex::Args->getopts;
+  return if $opts{'T'};
+  
   my @summary = Rex::TaskList->create()->get_summary();
   my @msgs    = ("SUMMARY");
 

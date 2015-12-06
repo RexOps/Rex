9b0bb9dc3fee2ca2d06e355213521f30fe83ad13
Eric Johnson <eric.git@iijo.org>
2015-12-06 01:24:38 -0500

    integration tests for exit codes

--- t/integration/exit_codes.t
+++ t/integration/exit_codes.t
@@ -1,7 +1,20 @@
 use strict;
 use warnings;
 use lib 't/lib';
+use t::Helper qw/test_rex /;
+use Test::More;
 
-use Test::Most;
+test_rex
+    args      => '-f aasdf97987',
+    exit_code => 1;
+
+test_rex
+    args      => 'bogus_task_name',
+    exit_code => 1;
+
+test_rex
+    args      => '-e "say run \"pwd\""',
+    exit_code => 0,
+    stderr    => qr/INFO - All tasks successful on all hosts/;
 
 done_testing;
---
+++ t/lib/t/Helper.pm
@@ -0,0 +1,57 @@
+package t::Helper;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw/Exporter/;
+our @EXPORT_OK = qw/test_rex logfile_regex/;
+
+use Test::Builder;
+use Test::Cmd;
+use Term::ANSIColor qw/colorstrip/;
+use Sys::Hostname qw/hostname/;
+
+sub datestrip {
+    my @lines = @_;
+    $_ =~ s/^\[\d\d\d\d-\d\d-\d\d \d\d\:\d\d\:\d\d\] // for @lines;
+    return join "", @lines;
+}
+
+sub test_rex {
+    my %params = @_;
+    $params{stdout} ||= qr/.*/;
+    $params{stderr} ||= qr/.*/;
+
+    my $args = $params{args} || die "args param is required";
+    my $perl = `which perl`;
+    chomp $perl;
+    my $prog = "$perl ./bin/rex";
+    my $cmd  = "${prog} ${args}";
+
+    my $builder = Test::Builder->new;
+
+    my $subtest = sub {
+        my $test = Test::Cmd->new(
+            prog    => $prog,
+            verbose => $params{verbose} // 0,
+            workdir => $params{workdir} // '',
+        );
+
+        $test->run(args => $args);
+
+        my $stdout = $test->stdout;
+        my $stderr = datestrip colorstrip $test->stderr;
+
+        print "STDOUT\n$stdout\n" if $params{debug};
+        print "STDERR\n$stderr\n" if $params{debug};
+
+        $builder->level(0); # Shouldn't have to do this?
+        $builder->like($stdout, qr|$params{stdout}|m // qr||, 'stdout');
+        $builder->like($stderr, qr|$params{stderr}|m // qr||, 'stderr');
+        $builder->is_eq($params{exit_code}, $? >> 8, 'exit code');
+    };
+
+    $builder->subtest($cmd, $subtest);
+}
+
+1;

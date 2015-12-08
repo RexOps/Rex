0cbb1f77e4aace4b04cbbbf497c9925f858e16f7
Ferenc Erki <erkiferenc@gmail.com>
2015-12-08 17:27:08 +0100

    Fix passing memory/cpus options to constructor

--- lib/Rex/Test/Base.pm
+++ lib/Rex/Test/Base.pm
@@ -88,8 +88,8 @@ sub new {
 
   $self->{name} ||= $file;
   $self->{redirect_port} = 2222;
-  $self->{memory}        = 512; # default, in MB
-  $self->{cpu}           = 1;   # default
+  $self->{memory} ||= 512; # default, in MB
+  $self->{cpu}    ||= 1;   # default
 
   return $self;
 }

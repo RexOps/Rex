12cb224029cb4a44f8dda94d5bb42c90ec4a9c29
Jan Gehring <krimdomu@googlemail.com>
2016-02-12 12:20:42 +0100

    added parameters to do_task and fixed LOCAL block

--- lib/Rex/Commands.pm
+++ lib/Rex/Commands.pm
@@ -691,14 +691,15 @@ You may also use an arrayRef for $task if you want to call multiple tasks.
 
 sub do_task {
   my $task = shift;
+  my $params = shift;
 
   if ( ref($task) eq "ARRAY" ) {
     for my $t ( @{$task} ) {
-      Rex::TaskList->run($t);
+      Rex::TaskList->run($t, ($params ? (params => $params) : ()));
     }
   }
   else {
-    return Rex::TaskList->run($task);
+    return Rex::TaskList->run($task, ($params ? (params => $params) : ()));
   }
 }
 
@@ -1173,7 +1174,7 @@ sub LOCAL (&) {
       ssh      => 0,
       server   => $cur_conn->{server},
       cache    => Rex::Interface::Cache->create(),
-      task     => task(),
+      task     => [task()],
       reporter => Rex::Report->create( Rex::Config->get_report_type ),
       notify   => Rex::Notify->new(),
     }

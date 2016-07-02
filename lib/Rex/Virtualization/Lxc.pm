e7f577d8a1e6cc398c5d1565feb77c7a160c3205
Oleg Hardt <litwol@litwol.com>
2016-07-03 01:19:49 +0200

    Implemented LXC container support

---
+++ lib/Rex/Virtualization/Lxc.pm
@@ -0,0 +1,53 @@
+#
+# (c) Oleg Hardt <litwol@litwol.com>
+#
+# vim: set ts=2 sw=2 tw=0:
+# vim: set expandtab:
+
+=head1 NAME
+
+Rex::Virtualization::Lxc - Linux Containers Virtualization Module
+
+=head1 DESCRIPTION
+
+With this module you can manage Linux Containers.
+
+=head1 SYNOPSIS
+
+ use Rex::Commands::Virtualization;
+
+ set virtualization => "Lxc";
+
+ use Data::Dumper;
+
+ print Dumper vm list => "all";
+ print Dumper vm list => "active",
+   fancy => 1,
+   format => 'name,ram';
+
+ print Dumper vm list => "all",
+ fancy => 1;
+
+=cut
+
+package Rex::Virtualization::Lxc;
+
+use strict;
+use warnings;
+
+# VERSION
+
+use Rex::Virtualization::Base;
+use base qw(Rex::Virtualization::Base);
+
+sub new {
+  my $that  = shift;
+  my $proto = ref($that) || $that;
+  my $self  = {@_};
+
+  bless( $self, $proto );
+
+  return $self;
+}
+
+1;
---
+++ lib/Rex/Virtualization/Lxc/info.pm
@@ -0,0 +1,46 @@
+#
+# (c) Oleg Hardt <litwol@litwol.com>
+#
+# vim: set ts=2 sw=2 tw=0:
+# vim: set expandtab:
+
+package Rex::Virtualization::Lxc::info;
+
+use strict;
+use warnings;
+
+# VERSION
+
+use Rex::Logger;
+use Rex::Helper::Run;
+
+sub execute {
+  my ( $class, $arg1 ) = @_;
+  my @dominfo;
+
+  if ( !$arg1 ) {
+    die('Must define container ID');
+  }
+
+  Rex::Logger::debug("Getting lxc-info");
+
+  my @container_info = i_run "lxc-info -n $arg1";
+  if ( $? != 0 ) {
+    die("Error running lxc-info");
+  }
+
+  my %ret;
+  for my $line (@container_info) {
+    my ( $column, $value ) = split( ':', $line );
+
+    # Trim white spaces.
+    $column =~ s/^\s+|\s+$//g;
+    $value =~ s/^\s+|\s+$//g;
+
+    $ret{$column} = $value;
+  }
+
+  return \%ret;
+}
+
+1;

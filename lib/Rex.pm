a716b49132cceec89ff1fe9357e497e24d75be55
krimdomu <krimdomu@users.noreply.github.com>
2017-09-29 20:44:51 +0200

    add Rexfile path to @INC, fixes bug with perl >=5.26 fixes #1170

--- lib/Rex.pm
+++ lib/Rex.pm
@@ -123,8 +123,10 @@ BEGIN {
 # this must come before all other paths, because custom libraries can be project dependant
 # see: #1108
     push @rex_inc, add_cwd_to_inc();
-
     push @rex_inc, add_libstruct_to_inc($_) for @additional;
+
+    # we have to add the Rexfile's path to @INC FIX: #1170
+    push @rex_inc, @additional;
 
 # add home directory/.rex/recipes to the search path, so that recipes can be managed
 # at a central location.

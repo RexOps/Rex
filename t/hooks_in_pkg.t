RexOps/Rex

commit 07f77a4a89e01e070a608384b29be849f5810954

tried to fix bug with before => ALL in modules

lib/Rex/TaskList/Base.pm
@@ -369,7 +369,7 @@ sub modify {
   my ( $self, $type, $task, $code, $package, $file, $line ) = @_;
 
   if ( $package ne "main" && $package ne "Rex::CLI" ) {
-    if ( $task !~ m/:/ ) {
+    if ( $task =~ m/:/ ) {
 
       #do we need to detect for base -Rex ?
       $package =~ s/^Rex:://;

t/before_all_tasks.t
@@ -1,0 +1,36 @@
+use strict;
+use warnings;
+use lib 't/lib';
+
+use Test::More;
+use Test::Deep;
+
+use Rex::Commands;
+use Rex::Commands::Run;
+
+use t::tasks::cowbefore;
+use Data::Dumper;
+
+$::QUIET = 1;
+
+my $task_list = Rex::TaskList->create;
+
+task 'eat' => sub { return 'delicious cows' };
+
+before ALL => sub { return 'yo' };
+
+my @task_names = $task_list->get_tasks;
+cmp_deeply
+  \@task_names,
+  [qw/eat t:tasks:cowbefore:roundup/],
+  "found visible task";
+
+for my $tn (@task_names) {
+
+  my $before = $task_list->get_task($tn)->get_data->{before};
+  ok($before);
+  is( ( scalar @$before ), 1, $tn );
+  is( $before->[0]->(), 'yo', $tn ) if (@$before);
+}
+
+done_testing();

t/lib/t/tasks/cowbefore.pm
@@ -1,0 +1,8 @@
+package t::tasks::cowbefore;
+use Rex -base;
+
+desc "bring in the cattle";
+task "roundup" => sub { return 1 };
+
+before ALL => sub { return 'yo' };
+1;

commit aa1e6a0dce1c0af11dd5533695e24195e70423dc

Fixes #1193 hooks don't register when defined in packages
This bug was introduced by 07f77a4a
See:
 $ git show 07f77a4a -- lib/Rex/TaskList/Base.pm

lib/Rex/TaskList/Base.pm
@@ -369,7 +369,7 @@ sub modify {
   my ( $self, $type, $task, $code, $package, $file, $line ) = @_;
 
   if ( $package ne "main" && $package ne "Rex::CLI" ) {
-    if ( $task =~ m/:/ ) {
+    if ( $task !~ m/:/ ) {
 
       #do we need to detect for base -Rex ?
       $package =~ s/^Rex:://;

t/hooks_in_pkg.t
@@ -14,21 +14,27 @@ $::QUIET = 1;
 
 my $task_list = Rex::TaskList->create;
 
-my @task_names = $task_list->get_tasks;
-cmp_deeply
-  \@task_names,
-  [qw/t:tasks:chicken:cross_road/],
-  "found task";
+my ($task_name) = $task_list->get_tasks;
+is( $task_name, "t:tasks:chicken:cross_road", "found_task" );
+my $task = $task_list->get_task($task_name);
 
-for my $tn (@task_names) {
-  require Data::Dumper;
-  Data::Dumper->import;
-  diag(Dumper($task_list->get_task($tn)->get_data));
-  my $before_task_start =
-    $task_list->get_task($tn)->get_data->{before_task_start};
-  is( ( scalar @$before_task_start ), 1, $tn );
-  is( $before_task_start->[0]->(), 'checked for traffic', $tn )
-    if (@$before_task_start);
-}
+my $bts = $task->{before_task_start};
+
+is( @$bts, 2, "found 2 before_task_start hooks" );
+is( ref $bts->[0] eq "CODE" ? $bts->[0]->() : undef,
+  "look left", "first before_task_start hook executes" );
+is( ref $bts->[1] eq "CODE" ? $bts->[1]->() : undef,
+  "look right", "second before_task_start hook executes" );
+
+my $atf = $task->{after_task_finished};
+
+is( @$atf, 2, "found 2 after_task_finished hooks" );
+is(
+  ref $atf->[0] eq "CODE" ? $atf->[0]->() : undef,
+  "got to the other side",
+  "first after_task_finished hook executes"
+);
+is( ref $atf->[1] eq "CODE" ? $atf->[1]->() : undef,
+  "celebrate!", "second after_task_finished hook executes" );
 
 done_testing();

t/lib/t/tasks/chicken.pm
@@ -4,6 +4,8 @@ use Rex -base;
 desc "cross the road";
 task "cross_road" => sub { return "make a break for it!" };
 
-before_task_start "cross_road"   => sub { return "checked for traffic" };
+before_task_start "cross_road"   => sub { return "look left" };
+before_task_start "cross_road"   => sub { return "look right" };
 after_task_finished "cross_road" => sub { return "got to the other side" };
+after_task_finished "cross_road" => sub { return "celebrate!" };
 1;
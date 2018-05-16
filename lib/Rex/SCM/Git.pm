880e71423c92320b8f6bf60a0b9dd6190c2cdb95
Joachim Bargsten <jw@bargsten.org>
2018-05-16 21:55:41 +0200

    perltidy

--- lib/Rex/SCM/Git.pm
+++ lib/Rex/SCM/Git.pm
@@ -30,18 +30,22 @@ sub checkout {
   my ( $self, $repo_info, $checkout_to, $checkout_opt ) = @_;
 
   my %run_opt;
-  $run_opt{env} = $checkout_opt->{env} if($checkout_opt->{env});
-  my $clone_args = join(" ", @{$checkout_opt->{clone_args} || ['']});
+  $run_opt{env} = $checkout_opt->{env} if ( $checkout_opt->{env} );
+  my $clone_args = join( " ", @{ $checkout_opt->{clone_args} || [''] } );
 
   if ( !is_dir($checkout_to) ) {
-    my $clone_cmd =
-      sprintf( $CLONE_COMMAND, $clone_args, $repo_info->{"url"}, basename($checkout_to) );
-    Rex::Logger::debug("clone_cmd: $clone_cmd (cwd: " . dirname($checkout_to). ")");
+    my $clone_cmd = sprintf( $CLONE_COMMAND,
+      $clone_args, $repo_info->{"url"}, basename($checkout_to) );
+    Rex::Logger::debug(
+      "clone_cmd: $clone_cmd (cwd: " . dirname($checkout_to) . ")" );
 
     Rex::Logger::info( "Cloning "
         . $repo_info->{"url"} . " to "
         . ( $checkout_to ? $checkout_to : "." ) );
-    my $out = i_run "$clone_cmd", cwd => dirname($checkout_to), fail_ok => 1, %run_opt;
+    my $out = i_run "$clone_cmd",
+      cwd     => dirname($checkout_to),
+      fail_ok => 1,
+      %run_opt;
     unless ( $? == 0 ) {
       Rex::Logger::info( "Error cloning repository.", "warn" );
       Rex::Logger::info($out);
@@ -100,7 +104,8 @@ sub checkout {
     my $rebase = $checkout_opt->{"rebase"} ? '--rebase' : '';
     my $out = i_run "git pull $rebase origin $branch",
       cwd     => $checkout_to,
-      fail_ok => 1, %run_opt;
+      fail_ok => 1,
+      %run_opt;
 
     unless ( $? == 0 ) {
       Rex::Logger::info( "Error pulling.", "warn" );
@@ -115,7 +120,10 @@ sub checkout {
       my $tag = $checkout_opt->{tag};
       my $checkout_cmd = sprintf( $CHECKOUT_TAG_COMMAND, $tag, $tag );
       Rex::Logger::info( "Switching to tag " . $tag );
-      $out = i_run "git fetch origin", cwd => $checkout_to, fail_ok => 1, %run_opt;
+      $out = i_run "git fetch origin",
+        cwd     => $checkout_to,
+        fail_ok => 1,
+        %run_opt;
 
       unless ( $? == 0 ) {
         Rex::Logger::info( "Error switching to tag.", "warn" );

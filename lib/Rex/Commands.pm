0c71980630d0da6e41f36fec6fef2c82854988f3
Ferenc Erki <erkiferenc@gmail.com>
2019-09-03 18:50:30 +0200

    perltidy

--- lib/Rex/Commands.pm
+++ lib/Rex/Commands.pm
@@ -759,8 +759,9 @@ sub run_task {
   my ( $task_name, %option ) = @_;
 
   my $task = Rex::TaskList->create()->get_task($task_name);
-  if(!$task) {
-    Rex::Logger::info( "Task $task_name not found, perhaps the name contains a typo?", "warn" );
+  if ( !$task ) {
+    Rex::Logger::info(
+      "Task $task_name not found, perhaps the name contains a typo?", "warn" );
     die("Error run_task $task_name");
   }
 

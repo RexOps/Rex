7472f5ad5a7d59b5a89feade716886ab57b0a10c
Ferenc Erki <erkiferenc@gmail.com>
2020-06-19 17:03:47 +0200

    Discontinue support for some EOL Windows versions

--- misc/check_supported_OS.pl
+++ misc/check_supported_OS.pl
@@ -1,7 +1,10 @@
 if ( $^O eq 'MSWin32' ) {
-  my ( undef, $major ) = Win32::GetOSVersion();
+  my ( undef, $major, $minor ) = Win32::GetOSVersion();
 
   if ( $major < 6 ) {
+    die 'OS unsupported';
+  }
+  elsif ( $major == 6 && $minor < 2 ) {
     die 'OS unsupported';
   }
 }

690f921093e6b6f6ac596fc2b76168aeed0af9b4
Ferenc Erki <erkiferenc@gmail.com>
2020-08-31 11:55:06 +0200

    Warn if cpuinfo is unreadable

--- lib/Rex/Inventory/Proc/Cpuinfo.pm
+++ lib/Rex/Inventory/Proc/Cpuinfo.pm
@@ -13,6 +13,7 @@ use warnings;
 
 use Data::Dumper;
 use Rex::Commands::File;
+use Rex::Commands::Fs;
 
 sub new {
   my $that  = shift;
@@ -27,21 +28,28 @@ sub new {
 sub get {
   my ($self) = @_;
 
-  my @cpuinfo = split /\n/, cat "/proc/cpuinfo";
-  chomp @cpuinfo;
-
   my @ret;
-  my $proc = 0;
-  for my $line (@cpuinfo) {
-    next if $line =~ qr{^$};
-    my ( $key, $val ) = split /\s*:\s*/, $line, 2;
-    if ( $key eq "processor" ) {
-      $proc = $val;
-      $ret[$proc] = {};
-      next;
-    }
 
-    $ret[$proc]->{$key} = $val;
+  if ( is_readable('/proc/cpuinfo') ) {
+
+    my @cpuinfo = split /\n/, cat "/proc/cpuinfo";
+    chomp @cpuinfo;
+
+    my $proc = 0;
+    for my $line (@cpuinfo) {
+      next if $line =~ qr{^$};
+      my ( $key, $val ) = split /\s*:\s*/, $line, 2;
+      if ( $key eq "processor" ) {
+        $proc = $val;
+        $ret[$proc] = {};
+        next;
+      }
+
+      $ret[$proc]->{$key} = $val;
+    }
+  }
+  else {
+    Rex::Logger::info( 'Cannot read /proc/cpuinfo', 'warn' );
   }
 
   return \@ret;

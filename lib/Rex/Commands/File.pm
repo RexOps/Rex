27b61f2cbcdc44b244fb7d5048466c7098d97af2
Ferenc Erki <erkiferenc@gmail.com>
2020-09-30 21:12:12 +0200

    Move declaration of default on_no_change action

--- lib/Rex/Commands/File.pm
+++ lib/Rex/Commands/File.pm
@@ -452,8 +452,9 @@ sub file {
   Rex::get_current_connection()->{reporter}
     ->report_resource_start( type => "file", name => $file );
 
-  my $need_md5  = ( $option->{"on_change"} && !$is_directory ? 1 : 0 );
-  my $on_change = $option->{"on_change"} || sub { };
+  my $need_md5     = ( $option->{"on_change"} && !$is_directory ? 1 : 0 );
+  my $on_change    = $option->{"on_change"}    || sub { };
+  my $on_no_change = $option->{"on_no_change"} || sub { };
 
   my $__ret = { changed => 0 };
 
@@ -744,7 +745,7 @@ sub file {
   elsif ( $__ret->{changed} == 0 ) {
     Rex::Logger::debug(
       "File $file has not been changed... Running on_no_change");
-    ( $option->{on_no_change} || sub { } )->($file);
+    &$on_no_change($file);
   }
 
   #### check and run after hook
